7629f6dae7784cb5698e00cb1fbe984ff667ab28
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-10-24 22:33:49 +0200

    Remove test items from index

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import { User } from "react-feather";
 
 export default function Home() {
   return (
@@ -10,13 +9,7 @@ export default function Home() {
       </Head>
 
       <div className="md:container mx-auto">
-        <div className="text-white text-center bg-gray-300 py-3 shadow-sm">
-          header
-        </div>
-        <div className="group flex justify-around items-center w-4/6 mx-auto py-2 mt-4 bg-white rounded-lg shadow-md hover:shadow-lg">
-          <User size={30} className="group-hover:text-black text-gray-600" />
-          <div className="group-hover:text-black text-gray-600">User</div>
-        </div>
+        <div className="bg-gray-300 h-12 shadow-sm"></div>
       </div>
     </div>
   );
