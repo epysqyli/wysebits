e24f344b6a7ce3cb2a40ee1aa399a75110cee2df
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-10-26 00:48:47 +0200

    Fix slider count behavior

--- components/Slider.jsx
+++ components/Slider.jsx
@@ -4,20 +4,37 @@ import HomeTileEntry from "./HomeTileEntry";
 
 const Slider = ({ entries }) => {
   const [count, setCount] = useState(0);
+  const upperLimit = entries.length - 1;
 
   const increment = () => {
-    setCount(count + 1);
+    if (count == upperLimit) {
+      setCount(0);
+    } else {
+      setCount(count + 1);
+    }
   };
 
   const decrement = () => {
-    setCount(count - 1);
+    if (count == 0) {
+      setCount(upperLimit);
+    } else {
+      setCount(count - 1);
+    }
   };
 
   return (
     <div className="flex justify-around items-center border rounded-md shadow-md w-5/6 mx-auto py-5">
-      <ArrowLeftCircle onClick={decrement} size={36} />
+      <ArrowLeftCircle
+        className="text-gray-500"
+        onClick={decrement}
+        size={30}
+      />
       <HomeTileEntry entry={entries[count]} />
-      <ArrowRightCircle onClick={increment} size={36} />
+      <ArrowRightCircle
+        className="text-gray-500"
+        onClick={increment}
+        size={30}
+      />
     </div>
   );
 };
