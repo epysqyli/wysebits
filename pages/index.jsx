cc03e30c8b6bd60311412ec114b4dd2b44ad3f6a
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-10-25 11:29:03 +0200

    Add homepage image banner

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -8,10 +9,18 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1 className="text-6xl font-bold mt-5 text-center">Wysebits.</h1>
-      <p className="text-center text-gray-500 text-2xl mt-5">
-        Knowledge. Distilled.
-      </p>
+      <div className="w-screen h-60 mx-auto bg-home-banner bg-cover bg-center">
+        <div className="bg-gray-900 h-full bg-opacity-40 relative">
+          <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+            <h1 className="text-white text-6xl font-bold text-center">
+              Wysebits.
+            </h1>
+            <p className="text-white text-center text-gray-500 text-2xl my-5">
+              Knowledge. Distilled.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
