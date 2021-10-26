dc4b26f90f27d368213f13e8574d69c95f0cc153
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-10-26 13:19:44 +0200

    Remove active style on mobile

--- components/HomeTileEntry.jsx
+++ components/HomeTileEntry.jsx
@@ -2,7 +2,7 @@ const HomeTileEntry = ({entry}) => {
   return (
     <div className="w-3/5">
       <div className="text-xl">{entry.book_tile.book.title}</div>
-      <p className="mt-5 text-justify">{entry.content}</p>
+      <p className="mt-5 text-justify">{entry.content.split(' ').slice(0, 30).join(' ')} ...</p>
       <div className="flex justify-between mt-5 pt-2 border-t">
         <span className="text-sm">Posted by: {entry.book_tile.user.name}</span>
         <span className="text-sm">{entry.book_tile.book.category.name}</span>
