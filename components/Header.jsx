b47818fe8a5d8cf52a985c3fe6f668aa0bb997f7
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-10-26 23:24:06 +0200

    Update header to check for logged user

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,27 @@
-import { LogIn } from "react-feather";
+import { LogIn, User } from "react-feather";
 import Link from "next/link";
 
-const Header = () => {
+const Header = ({ userState }) => {
+  const login = (
+    <Link href="/registrations/login">
+      <a className="flex items-center group cursor-pointer">
+        <LogIn size={20} className="group-hover:scale-110 transition-all" />
+        <div className="mx-3 group-active:text-white transition-all">Login</div>
+      </a>
+    </Link>
+  );
+
+  const loggedUser = (
+    <Link href="/user/homepage">
+      <a className="flex items-center group cursor-pointer">
+        <User size={20} className="group-hover:scale-110 transition-all" />
+        <div className="mx-3 group-active:text-white transition-all">
+          {userState.user.username}
+        </div>
+      </a>
+    </Link>
+  );
+
   return (
     <header className="flex justify-between items-center bg-gray-300 py-2 px-5 text-center w-screen fixed z-10 shadow-sm">
       <Link href="/">
@@ -11,14 +31,7 @@ const Header = () => {
           </div>
         </a>
       </Link>
-      <Link href="/registrations/login">
-        <a className="flex items-center group cursor-pointer">
-          <LogIn size={20} className="group-hover:scale-110 transition-all" />
-          <div className="mx-3 group-active:text-white transition-all">
-            Login
-          </div>
-        </a>
-      </Link>
+      {userState.user ? loggedUser : login}
     </header>
   );
 };
--- layouts/layout.jsx
+++ layouts/layout.jsx
@@ -1,10 +1,10 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
-const Layout = ({ children }) => {
+const Layout = ({ children, userState }) => {
   return (
     <>
-      <Header />
+      <Header userState={userState} />
       <main>{children}</main>
       <Footer />
     </>
