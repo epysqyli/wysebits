7dbc49e5ce632e43bd14683b70a43eccfee01610
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-10-26 13:46:13 +0200

    Add thumbs up and down count on HomeTileEntry

--- components/HomeTileEntry.jsx
+++ components/HomeTileEntry.jsx
@@ -1,11 +1,29 @@
-const HomeTileEntry = ({entry}) => {
+import { ThumbsUp, ThumbsDown } from "react-feather";
+
+const HomeTileEntry = ({ entry }) => {
   return (
     <div className="w-3/5">
       <div className="text-xl">{entry.book_tile.book.title}</div>
-      <p className="mt-5 text-justify">{entry.content.split(' ').slice(0, 30).join(' ')} ...</p>
-      <div className="flex justify-between mt-5 pt-2 border-t">
-        <span className="text-sm">Posted by: {entry.book_tile.user.name}</span>
-        <span className="text-sm">{entry.book_tile.book.category.name}</span>
+      <p className="mt-5 text-justify">
+        {entry.content.split(" ").slice(0, 30).join(" ")} ...
+      </p>
+      <div className="flex justify-between items-center mt-5 pt-2 border-t">
+        <div>
+          <p className="text-sm">Posted by: {entry.book_tile.user.name}</p>
+          <p className="text-sm">
+            Category: {entry.book_tile.book.category.name}
+          </p>
+        </div>
+        <div>
+          <div className="flex justify-between items-center">
+            <ThumbsUp size={14} />
+            <span className="ml-3 text-sm">{entry.upvotes}</span>
+          </div>
+          <div className="flex justify-between items-center">
+            <ThumbsDown size={14} />
+            <span className="ml-3 text-sm">{entry.downvotes}</span>
+          </div>
+        </div>
       </div>
     </div>
   );
