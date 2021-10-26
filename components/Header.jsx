f6bdc511c42fdf0553d09d07d8e70904face7756
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-10-26 17:29:16 +0200

    Add form on signup

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,11 +1,16 @@
 import { LogIn } from "react-feather";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <header className="flex justify-between items-center bg-gray-300 py-2 px-5 text-center w-screen fixed z-10 shadow-sm">
-      <div className="text-2xl font-bold cursor-pointer hover:scale-105 transition-all active:text-white">
-        Wbits.
-      </div>
+      <Link href="/">
+        <a>
+          <div className="text-2xl font-bold cursor-pointer hover:scale-105 transition-all active:text-white">
+            Wbits.
+          </div>
+        </a>
+      </Link>
       <div className="flex items-center group cursor-pointer">
         <LogIn size={20} className="group-hover:scale-110 transition-all" />
         <div className="mx-3 group-active:text-white">Login</div>
