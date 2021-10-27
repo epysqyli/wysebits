33397ec9f62b75bfc7c950283a93835140aae9d1
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-10-27 13:42:25 +0200

    Change index bottom banner

--- pages/index.jsx
+++ pages/index.jsx
@@ -136,7 +136,11 @@ const Home = ({ entries, categories, loginStatus, userState }) => {
 
       <div className="h-60 mx-auto bg-bottom-home bg-cover bg-center">
         <div className="bg-gray-800 h-full bg-opacity-80 relative">
-          {userState.isLogged ? null : (
+          {userState.isLogged ? (
+            <div className="w-4/6 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+              <div className="text-3xl font-bold text-gray-200 text-center">Wbits.</div>
+            </div>
+          ) : (
             <div className="w-4/6 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
               <Link href="/registrations/signup">
                 <a>
--- pages/user/homepage.jsx
+++ pages/user/homepage.jsx
@@ -1,4 +1,4 @@
-const Homepage = () => {
+const Homepage = ({ userState }) => {
   return (
     <div className="mt-20 text-center">
       Welcome to your user page. It is time to do great things.
