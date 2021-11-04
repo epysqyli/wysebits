a671f47e71fde97a9f038a0e1b95d558d447dd05
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-11-04 16:56:02 +0100

    Add OL cover images to book results, no image case to be fixed

--- components/BookResult.jsx
+++ components/BookResult.jsx
@@ -1,13 +1,19 @@
 const BookResult = ({ bookData }) => {
   return (
-    <div className="border rounded-md shadow-sm my-10 px-5">
-      <div className="text-xl text-center">{bookData._source.title}</div>
-      <div className="flex justify-around mt-5">
-        <div>{bookData._source.category.name}</div>
-        <div className="text-right">
-          {bookData._source.authors.length
-            ? bookData._source.authors[0].full_name
-            : "No authors found"}
+    <div className="border rounded-md shadow-sm my-10 px-3 py-5">
+      <div className="flex justify-between">
+        <img
+          className="w-2/6"
+          src={`https://covers.openlibrary.org/w/olid/${bookData._source.ol_key}-M.jpg`}
+        />
+        <div className="ml-5">
+          <div className="text-xl">{bookData._source.title}</div>
+          <div>{bookData._source.category.name}</div>
+          <div>
+            {bookData._source.authors.length
+              ? bookData._source.authors[0].full_name
+              : "No authors found"}
+          </div>
         </div>
       </div>
     </div>
