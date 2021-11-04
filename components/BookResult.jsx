fc9efa1520434290cbe5dae7a4ccc7c376fa5d11
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-11-04 19:21:39 +0100

    Add naive and inefficient loading on BookResult image

--- components/BookResult.jsx
+++ components/BookResult.jsx
@@ -1,4 +1,20 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
 const BookResult = ({ bookData }) => {
+  const [cover, setCover] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://covers.openlibrary.org/w/olid/${bookData._source.ol_key}-M.jpg`
+      )
+      .then((resp) => {
+        setCover(resp.data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
   return (
     <div className="border rounded-md shadow-sm my-10 px-5 py-5 cursor-pointer hover:shadow-md transition hover:bg-gray-100">
       <div className="flex justify-between">
@@ -8,16 +24,21 @@ const BookResult = ({ bookData }) => {
             dangerouslySetInnerHTML={{ __html: bookData.highlight.title }}
           ></div>
           <div className="text-sm">{bookData._source.category.name}</div>
-          <div className="text-sm">
+          <div className="text-sm italic">
             {bookData._source.authors.length
               ? bookData._source.authors[0].full_name
               : "No authors found"}
           </div>
         </div>
-        <img
-          className="w-2/6 rounded-md"
-          src={`https://covers.openlibrary.org/w/olid/${bookData._source.ol_key}-M.jpg`}
-        />
+
+        {!cover ? (
+          <div>loading ...</div>
+        ) : (
+          <img
+            className="w-2/6 rounded-md"
+            src={`https://covers.openlibrary.org/w/olid/${bookData._source.ol_key}-M.jpg`}
+          />
+        )}
       </div>
     </div>
   );
