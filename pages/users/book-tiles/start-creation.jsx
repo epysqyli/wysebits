6f6c8c715944c9cddac6c4473d7eda69ca2231e9
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-11-05 18:52:29 +0100

    Add animation to start-creation bottom button

--- pages/users/book-tiles/start-creation.jsx
+++ pages/users/book-tiles/start-creation.jsx
@@ -7,12 +7,16 @@ import BookResult from "../../../components/BookResult";
 const Create = () => {
   const [searchTerms, setSearchTerms] = useState(null);
   const [searchResults, setSearchResults] = useState([]);
-  const [showBtn, setShowBtn] = useState(false);
+  const [btnVisible, setBtnVisible] = useState(false);
 
   const handleChange = (e) => {
     const newSearchTerms = e.target.value;
     setSearchTerms(newSearchTerms);
   };
+
+  const showBtn = () => {
+    setBtnVisible(true);
+  }
 
   const searchBooks = (e) => {
     e.preventDefault();
@@ -28,12 +32,14 @@ const Create = () => {
       })
       .catch((error) => console.log(error));
 
-    setShowBtn(true);
+    setTimeout(showBtn, 2000);
   };
 
   const createBookBtn = (
-    <div className="cursor-pointer py-5 text-center transition bg-gray-100 hover:bg-gray-300 active:bg-gray-400 fixed bottom-0 left-0 w-screen">
-      <div className="text-sm font-medium uppercase">Book not there? Create it!</div>
+    <div className="cursor-pointer py-5 text-center transition bg-gray-100 hover:bg-gray-300 active:bg-gray-400 fixed bottom-0 left-0 w-screen animate-show-up">
+      <div className="text-sm font-medium uppercase">
+        Book not there? Create it!
+      </div>
     </div>
   );
 
@@ -63,7 +69,7 @@ const Create = () => {
             type="submit"
             className="w-1/6 text-center bg-white border-l rounded-tr-lg rounded-br-lg hover:bg-gray-200 group-hover:shadow-md transition active:bg-gray-400 active:text-white"
           >
-            <Search size={20} className="mx-auto"/>
+            <Search size={20} className="mx-auto" />
           </button>
         </form>
 
@@ -73,7 +79,7 @@ const Create = () => {
             })
           : null}
 
-        {showBtn ? createBookBtn : null}
+        {btnVisible ? createBookBtn : null}
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const { keyframes } = require("tailwindcss/defaultTheme");
+
 module.exports = {
   mode: "jit",
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
@@ -6,10 +8,19 @@ module.exports = {
     extend: {
       backgroundImage: {
         "bottom-home": "url('/images/ai-banner.jpg')",
-        "library": "url('/images/library.jpg')",
+        library: "url('/images/library.jpg')",
         "signup-top": "url('/images/signup-top.jpg')",
         "login-top": "url('/images/progress.jpg')",
         "create-book-tile": "url('/images/create-book-tile.jpg')",
+      },
+      animation: {
+        "show-up": "appear 300ms ease-out 1",
+      },
+      keyframes: {
+        appear: {
+          "0%": { opacity: 0.1 },
+          "100%": { opacity: 1 },
+        },
       },
     },
   },
