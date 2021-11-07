46ee9d54aa53d3345bbbdcee12f0bf03e8c56a73
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-11-08 00:35:05 +0100

    Add uploaded image url display on book-tile-creation [id]

--- pages/users/book-tiles/book-tile-creation/[id].jsx
+++ pages/users/book-tiles/book-tile-creation/[id].jsx
@@ -5,18 +5,23 @@ export const getServerSideProps = async ({ params }) => {
   return {
     props: {
       book: res.data,
+      image_url: res.image_url,
     },
   };
 };
 
-const CreateBookTile = ({ book }) => {
+const CreateBookTile = ({ book, image_url }) => {
   return (
     <div className="w-4/5 mx-auto my-20">
       <div className="flex justify-between p-2 border bg-gray-100 rounded-md shadow-sm">
-        <img
-          className="w-2/6 rounded-md bg-gray-200"
-          src={`https://covers.openlibrary.org/w/olid/${book.ol_key}-M.jpg`}
-        />
+        {image_url ? (
+          <img className="w-2/6 rounded-md bg-gray-200" src={image_url} />
+        ) : (
+          <img
+            className="w-2/6 rounded-md bg-gray-200"
+            src={`https://covers.openlibrary.org/w/olid/${book.ol_key}-M.jpg`}
+          />
+        )}
         <div className="w-3/6">
           <div className="text-xl mb-5 font-medium">{book.title}</div>
           <div className="text-sm">{book.category.name}</div>
@@ -26,7 +31,9 @@ const CreateBookTile = ({ book }) => {
         </div>
       </div>
 
-      <div className="text-xl text-center mt-10">What are the top 3 takeways for you?</div>
+      <div className="text-xl text-center mt-10">
+        What are the top 3 takeways for you?
+      </div>
     </div>
   );
 };
