73eb01e9fa4cb646d0ba1e0df4290ff406a36da7
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-11-08 16:58:06 +0100

    Edit base structure of [id]

--- pages/users/book-tiles/book-tile-creation/[id].jsx
+++ pages/users/book-tiles/book-tile-creation/[id].jsx
@@ -21,21 +21,29 @@ const CreateBookTile = ({ book }) => {
   );
 
   return (
-    <div className="w-4/5 mx-auto my-20">
-      <div className="flex justify-between p-2 border bg-gray-100 rounded-md shadow-sm">
-        {coverImage}
-        
-        <div className="w-3/6">
-          <div className="text-xl mb-5 font-medium">{book.title}</div>
-          <div className="text-sm">{book.category.name}</div>
-          <div className="text-sm italic">
-            {book.authors[0] ? book.authors[0].full_name : "No authors found"}
+    <div>
+      <div className="w-4/5 mx-auto mt-20 mb-10">
+        <div className="flex justify-between p-2 border bg-gray-100 rounded-md shadow-sm">
+          {coverImage}
+
+          <div className="w-3/6">
+            <div className="text-xl mb-5 font-medium">{book.title}</div>
+            <div className="text-sm">{book.category.name}</div>
+            <div className="text-sm italic">
+              {book.authors[0] ? book.authors[0].full_name : "No authors found"}
+            </div>
           </div>
         </div>
       </div>
 
-      <div className="text-xl text-center mt-10">
-        What are the top 3 takeways for you?
+      <div className="text-2xl text-center mb-10 py-3 bg-gray-200">
+        <div className="bg-gray-100 w-4/5 mx-auto rounded py-2 px-2 shadow-md">
+          Share the top 3 takeaways with the world!
+        </div>
+      </div>
+
+      <div className="w-4/5 mx-auto my-10">
+        <form>{/* form creates a book tile and posts the tile-entries */}</form>
       </div>
     </div>
   );
