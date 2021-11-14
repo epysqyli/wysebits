a1214b128627ce2ac725ae8ac381be6259d74669
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-11-14 18:30:12 +0100

    Remove required attributes on edit form

--- components/users/EditBookDetails.jsx
+++ components/users/EditBookDetails.jsx
@@ -7,12 +7,17 @@ const EditBookDetails = ({ bookData, categories }) => {
     author: bookData.authors[0] || null,
   });
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("prevented");
+  };
+
   return (
     <div className="absolute top-1/8 left-1/2 bg-white p-3 -translate-x-1/2 w-11/12 rounded-md shadow-lg border-2 border-gray-400 animate-show-up">
-      <div className="text-center">
-        Edit book infos and make wysebits a better place for all!
+      <div className="text-center border-b-2 pb-2">
+        Edit book infos and make Wysebits a better place for all!
       </div>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="my-10">
           <label htmlFor="title" className="pl-3">
             Enter the book title
@@ -23,7 +28,6 @@ const EditBookDetails = ({ bookData, categories }) => {
             id="title"
             className="border-none bg-white w-full mt-2 rounded-md focus:ring-0 shadow-sm focus:shadow-md"
             placeholder="Book title"
-            required
           />
         </div>
 
@@ -36,7 +40,6 @@ const EditBookDetails = ({ bookData, categories }) => {
             id="category"
             className="border-none bg-white w-full mt-2 rounded-md focus:ring-0 shadow-sm"
             defaultValue={bookData.category_id}
-            required
           >
             {categories.map((category) => (
               <option value={category.id} key={category.id}>
@@ -56,16 +59,15 @@ const EditBookDetails = ({ bookData, categories }) => {
             id="author-full-name"
             className="border-none bg-white w-full mt-2 rounded-md focus:ring-0 shadow-sm focus:shadow-md"
             placeholder="Enter the full name"
-            required
           />
         </div>
+        <button
+          type="submit"
+          className="w-3/5 mx-auto block mt-10 mb-5 py-2 bg-gray-100 rounded-md hover:shadow-md hover:bg-gray-200 active:bg-gray-300 active:shadow-md"
+        >
+          Edit book details
+        </button>
       </form>
-      <button
-        type="submit"
-        className="w-3/5 mx-auto block mt-10 mb-5 py-2 bg-gray-100 rounded-md hover:shadow-md hover:bg-gray-200 active:bg-gray-300 active:shadow-md"
-      >
-        Edit book details
-      </button>
     </div>
   );
 };
