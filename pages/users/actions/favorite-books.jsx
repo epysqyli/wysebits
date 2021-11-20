5c1fc0ba9aa9ce8591d3b56eb4b79f1fa2c91537
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-11-20 18:53:29 +0100

    Implement basic favorite-books page

--- pages/users/actions/favorite-books.jsx
+++ pages/users/actions/favorite-books.jsx
@@ -1,11 +1,36 @@
-export const getServerSideProps = () => {
+import axios from "axios";
+import BookCard from "../../../components/BookCard";
+
+export const getServerSideProps = async (context) => {
+  const userResp = await axios({
+    method: "get",
+    url: "http://localhost:3001/api/logged_in",
+    headers: { cookie: context.req.headers.cookie },
+  });
+
+  const favBooks = await axios({
+    method: "get",
+    url: `http://localhost:3001/api/users/${userResp.data.user.id}/fav_books`,
+    headers: { cookie: context.req.headers.cookie },
+  });
+
   return {
-    props: {},
+    props: { books: favBooks.data },
   };
 };
 
-const FavoriteBooks = () => {
-  return <div></div>;
+const FavoriteBooks = ({ books }) => {
+  return (
+    <div className="mt-20">
+      {books.map((book) => {
+        return (
+          <div className="border rounded-md my-10 w-4/5 mx-auto bg-gray-100">
+            <BookCard bookData={book} key={book.id} />
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default FavoriteBooks;
