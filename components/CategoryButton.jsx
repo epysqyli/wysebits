3164d449b1dd4d86ee8484e8bd9e7eaeb7b549fe
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-11-21 20:37:45 +0100

    Add categories

--- components/CategoryButton.jsx
+++ components/CategoryButton.jsx
@@ -1,8 +1,12 @@
+import Link from "next/dist/client/link";
+
 const CategoryButton = ({ category }) => {
   return (
-    <div className="text-center p-2 rounded-md shadow-md cursor-pointer bg-white hover:bg-gray-100 hover:shadow-md active:bg-gray-200 transition-all">
-      {category.name}
-    </div>
+    <Link href={`/categories/${category.slug}`}>
+      <div className="text-center p-2 rounded-md shadow-md cursor-pointer bg-white hover:bg-gray-100 hover:shadow-md active:bg-gray-200 transition-all">
+        {category.name}
+      </div>
+    </Link>
   );
 };
 
---
+++ pages/categories/[category].jsx
@@ -0,0 +1,40 @@
+import axios from "axios";
+import WelcomeTop from "../../components/users/WelcomeTop";
+import BookCard from "../../components/BookCard";
+
+export const getServerSideProps = async (context) => {
+  const slug = context.query.category;
+  const categoryName = slug.split("-").join(" ");
+
+  const books = await axios.get(
+    `http://localhost:3001/api/categories/${slug}/books`
+  );
+
+  return {
+    props: { books: books.data, categoryName },
+  };
+};
+
+const Category = ({ books, categoryName }) => {
+  const capitalize = (str) => {
+    return `${str.slice(0, 1).toUpperCase()}${str.slice(1)}`;
+  };
+
+  return (
+    <div>
+      <WelcomeTop firstLine={capitalize(categoryName)} />
+
+      <div className="w-4/5 mx-auto my-10">
+        {books.map((book) => {
+          return (
+            <div className="my-10 border rounded-md bg-gray-100 hover:bg-gray-200 animate-show-up cursor-pointer active:bg-gray-300">
+              <BookCard bookData={book} key={book.id} />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Category;
