876b1c4079c59658dc39f9c3884ffbb9b5265c91
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-11-21 19:47:22 +0100

    Change to WelcomeTop component

--- components/users/WelcomeTop.jsx
+++ components/users/WelcomeTop.jsx
@@ -1,6 +1,6 @@
 const WelcomeTop = ({ firstLine, secondLine }) => {
   return (
-    <div className="text-2xl mt-16 text-center border-b-2 pb-2 shadow-md">
+    <div className="text-2xl mt-12 text-center bg-white border py-5 shadow-md">
       <div>{firstLine}</div>
       <div className="mt-2">{secondLine}</div>
     </div>
