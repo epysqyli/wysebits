611f10d5c1b025f6a92b425ae3f7b90a0a4314a4
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-11-21 21:01:36 +0100

    Add margin-top to globals style

--- components/users/WelcomeTop.jsx
+++ components/users/WelcomeTop.jsx
@@ -1,6 +1,6 @@
 const WelcomeTop = ({ firstLine, secondLine }) => {
   return (
-    <div className="text-2xl mt-12 text-center bg-white border py-5 shadow-md">
+    <div className="text-2xl text-center bg-white py-5 shadow-md">
       <div>{firstLine}</div>
       <div className="mt-2">{secondLine}</div>
     </div>
