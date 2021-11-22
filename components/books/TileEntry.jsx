6383da1cd054ef39894750e225d920b70ab194f2
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-11-23 00:05:48 +0100

    Add tile entries

--- components/books/TileEntry.jsx
+++ components/books/TileEntry.jsx
@@ -1,6 +1,30 @@
+import { ThumbsUp, ThumbsDown } from "react-feather";
+
 const TileEntry = ({ data }) => {
   return (
-    <div className="w-4/5 mx-auto text-justify leading-7">{data.content}</div>
+    <div className="w-4/5 mx-auto text-justify leading-7">
+      <div className="border-b-2 pb-2">{data.content}</div>
+
+      <div className="flex justify-between items-center text-sm px-1">
+        <div className="flex justify-center gap-x-6">
+          <div className="flex items-center gap-x-1 py-2">
+            <div>{data.upvotes}</div>
+            <div className="pb-1">
+              <ThumbsUp size={16} />
+            </div>
+          </div>
+
+          <div className="flex items-center gap-x-1">
+            <div>{data.downvotes}</div>
+            <div>
+              <ThumbsDown size={16} />
+            </div>
+          </div>
+        </div>
+
+        <div>{data.book_tile.user.username}</div>
+      </div>
+    </div>
   );
 };
 
--- pages/books/[slug].jsx
+++ pages/books/[slug].jsx
@@ -9,14 +9,18 @@ export const getServerSideProps = async (context) => {
     `http://localhost:3001/api/books/${id}/tile_entries`
   );
 
+  const title = slug.slice(0, slug.length - 1).join(" ");
+  const capTitle = title.slice(0, 1).toUpperCase() + title.slice(1);
+
   return {
-    props: { entries: entries.data[0] },
+    props: { entries: entries.data[0], title: capTitle },
   };
 };
 
-const Book = ({ entries }) => {
+const Book = ({ entries, title }) => {
   return (
     <div className="w-4/5 mx-auto">
+      <div className="text-center mt-10 text-2xl font-bold">{title}</div>
       {entries.map((entry) => {
         return (
           <div className="my-10">
