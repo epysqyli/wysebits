4214fe30b36b7ecf0c232d9a1164783861b87f31
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-11-22 22:57:32 +0100

    Add animate-show-up-slow to BookCard coverImage upon loading

--- components/BookCard.jsx
+++ components/BookCard.jsx
@@ -16,7 +16,7 @@ const BookCard = ({ bookData }) => {
 
   const coverImage = (
     <img
-      className="w-2/6 rounded-md bg-gray-300 object-cover"
+      className="w-2/6 rounded-md bg-gray-300 object-cover animate-show-up-slow"
       src={dbSrc || olSrc || ""}
     />
   );
