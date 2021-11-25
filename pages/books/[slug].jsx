dfe32f1a13bb9f589405f33127f56a9a96d86f15
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-11-25 21:53:34 +0100

    Fix books/[slug]

--- pages/books/[slug].jsx
+++ pages/books/[slug].jsx
@@ -7,6 +7,8 @@ export const getServerSideProps = async (context) => {
   const slug = context.query.slug.split("-");
   const id = slug[slug.length - 1];
 
+  const book = await axios(`http://localhost:3001/api/books/${id}`);
+
   const entries = await axios(
     `http://localhost:3001/api/books/${id}/tile_entries`
   );
@@ -16,16 +18,16 @@ export const getServerSideProps = async (context) => {
 
   if (entries.data.length != 0) {
     return {
-      props: { entries: entries.data[0], title: capTitle },
+      props: { entries: entries.data[0], title: capTitle, book: book.data },
     };
   } else {
     return {
-      props: { entries: false, title: capTitle, bookId: id },
+      props: { entries: false, title: capTitle, book: book.data },
     };
   }
 };
 
-const Book = ({ entries, title, bookId }) => {
+const Book = ({ entries, title, book }) => {
   if (entries) {
     return (
       <div className="w-4/5 mx-auto animate-show-up">
@@ -44,12 +46,16 @@ const Book = ({ entries, title, bookId }) => {
       <div className="w-4/5 mx-auto animate-show-up">
         <div className="text-center mt-10 text-2xl font-bold">{title}</div>
         <div className="flex justify-between items-center my-20 px-3">
-          <Info size={36} strokeWidth={1.5} className="bg-gray-200 rounded-3xl"/>
+          <Info
+            size={36}
+            strokeWidth={1.5}
+            className="bg-gray-200 rounded-3xl"
+          />
           <div className="w-4/5 mx-auto">
             It appears as though there are no insights for this book yet ...
           </div>
         </div>
-        <Link href={`/users/book-tiles/create/${bookId}`}>
+        <Link href={`/users/book-tiles/create/${book.id}`}>
           <div className="border px-5 py-3 bg-gray-100 rounded-md shadow group transition-all cursor-pointer hover:shadow-md">
             Be the first and share your knowledge by adding your own insights
             for <b>{title}</b> now!
