91fe8dc1e84cd739d3c050a20c081a14f81f6691
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-11-27 14:35:11 +0100

    Update NoAccess component style

--- components/users/NoAccess.jsx
+++ components/users/NoAccess.jsx
@@ -5,19 +5,19 @@ const NoAccess = () => {
     <div>
       <div className="w-4/5 mx-auto mt-20">
         <Link href="/registrations/signup">
-          <a className="block text-2xl text-center border rounded-lg mx-auto py-10 px-2 my-10 cursor-pointer shadow-md bg-white hover:shadow-lg active:bg-gray-100">
-            Sign up to have access to this area
+          <a className="block text-xl text-center border rounded-lg mx-auto py-5 px-10 my-10 cursor-pointer shadow-md bg-white hover:shadow-lg active:bg-gray-100">
+            Sign up to start contributing to Wysebits!
           </a>
         </Link>
 
         <Link href="/registrations/login">
-          <a className="block text-2xl text-center border rounded-lg mx-auto py-10 px-2 my-10 cursor-pointer shadow-md bg-white hover:shadow-lg active:bg-gray-100">
+          <a className="block text-xl text-center border rounded-lg mx-auto py-10 px-2 my-10 cursor-pointer shadow-md bg-white hover:shadow-lg active:bg-gray-100">
             Login if already registered
           </a>
         </Link>
 
         <Link href="/">
-          <a className="block text-2xl text-center border rounded-lg mx-auto py-10 px-2 my-10 cursor-pointer shadow-md bg-white hover:shadow-lg active:bg-gray-100">
+          <a className="block text-xl text-center border rounded-lg mx-auto py-10 px-2 my-10 cursor-pointer shadow-md bg-white hover:shadow-lg active:bg-gray-100">
             Keep exploring as a guest
           </a>
         </Link>
