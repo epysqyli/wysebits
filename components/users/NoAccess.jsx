b25add01af1f67f428afa84c1edffa9da1180607
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-11-27 15:33:39 +0100

    Improve NoAccess component

--- components/users/NoAccess.jsx
+++ components/users/NoAccess.jsx
@@ -1,12 +1,21 @@
 import Link from "next/link";
+import { AlertCircle } from "react-feather";
 
 const NoAccess = () => {
   return (
     <div>
+      <div className="w-4/5 mx-auto mt-20 flex justify-around items-center">
+        <AlertCircle className="w-1/6" size={36} strokeWidth={1.5} />
+        <div className="w-4/6">
+          You have no permission to access this page.
+          <br /> Fear not! You have three options:
+        </div>
+      </div>
+
       <div className="w-4/5 mx-auto mt-20">
         <Link href="/registrations/signup">
           <a className="block text-xl text-center border rounded-lg mx-auto py-5 px-10 my-10 cursor-pointer shadow-md bg-white hover:shadow-lg active:bg-gray-100">
-            Sign up to start contributing to Wysebits!
+            Sign up to start contributing to Wysebits today!
           </a>
         </Link>
 
