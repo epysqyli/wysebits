2922ae684e76c272781528ba8b7c11fc1dbe4f30
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-11-28 21:46:09 +0100

    Change Link in TileEntryWithTitle

--- components/books/TileEntryWithTitle.jsx
+++ components/books/TileEntryWithTitle.jsx
@@ -8,20 +8,19 @@ const TileEntryWithTitle = ({ data }) => {
 
   return (
     <div className="mx-auto bg-white text-justify leading-6 shadow rounded hover:shadow-md transition-shadow">
-      <div className="px-2 py-2 flex justify-center items-center gap-x-5">
-        <div className="text-sm text-gray-600">{data.book_tile.book.title}</div>
-        <Link
-          href={`/books/${slug(
-            data.book_tile.book.title,
-            data.book_tile.book.id
-          )}`}
-        >
-          <ArrowUpRight
-            size={18}
-            className="text-gray-500 hover:text-gray-800 hover:scale-125 cursor-pointer transition-transform"
-          />
-        </Link>
-      </div>
+      <Link
+        href={`/books/${slug(
+          data.book_tile.book.title,
+          data.book_tile.book.id
+        )}`}
+      >
+        <div className="px-2 py-2 flex justify-center items-center gap-x-5 hover:scale-105 active:text-black transition-transform cursor-pointer">
+          <div className="text-sm text-gray-600">
+            {data.book_tile.book.title}
+          </div>
+          <ArrowUpRight size={18} className="text-gray-600" />
+        </div>
+      </Link>
       <div className="border-b-2 py-3 px-2">{data.content}</div>
 
       <div className="flex justify-between items-center text-sm px-2 py-2">
