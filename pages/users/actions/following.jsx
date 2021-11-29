738ce931913c93730c7d91113c679a916371b59d
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-11-29 23:14:32 +0100

    Add data fetching to following for users

--- pages/users/actions/following.jsx
+++ pages/users/actions/following.jsx
@@ -1,6 +1,27 @@
 import WelcomeTop from "../../../components/users/WelcomeTop";
+import axios from "axios";
 
-const Following = () => {
+export const getServerSideProps = async (context) => {
+  const userResp = await axios({
+    method: "get",
+    url: "http://localhost:3001/api/logged_in",
+    headers: { cookie: context.req.headers.cookie },
+  });
+
+  const resp = await axios({
+    method: "get",
+    url: `http://localhost:3001/api/users/${userResp.data.user.id}/following`,
+    headers: { cookie: context.req.headers.cookie },
+  });
+
+  return {
+    props: {
+      following: resp.data,
+    },
+  };
+};
+
+const Following = ({ following }) => {
   return (
     <div>
       <WelcomeTop text="Users you are following" bcgImg="bg-following" />
