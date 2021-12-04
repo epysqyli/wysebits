824cf050ea8eb36f17d1b0c73053cd6ffdbec249
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-12-04 15:14:59 +0100

    Add style to PageNavButton

--- components/navigation/PageNavButton.jsx
+++ components/navigation/PageNavButton.jsx
@@ -1,14 +1,23 @@
 import Link from "next/dist/client/link";
-import { useEffect } from "react";
 
 const PageNavButton = ({ btnText, url, categorySlug }) => {
   const pageNum = url.split("?page=")[1] || "";
 
-  return (
-    <Link href={`/categories/${categorySlug}/${pageNum}`}>
-      <div className="py-2 text-center rounded border shadow">{btnText}</div>
-    </Link>
-  );
+  if (pageNum == "") {
+    return (
+      <div className="py-2 text-center text-gray-300 rounded shadow">
+        {btnText}
+      </div>
+    );
+  } else {
+    return (
+      <Link href={`/categories/${categorySlug}/${pageNum}`}>
+        <div className="py-2 text-center rounded border shadow cursor-pointer hover:bg-gray-200 active:bg-gray-300 active:text-white transition-colors">
+          {btnText}
+        </div>
+      </Link>
+    );
+  }
 };
 
 export default PageNavButton;
