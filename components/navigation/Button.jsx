ff04495950990967bf8d1843f570f912dafb3094
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-12-11 18:24:17 +0100

    Partially restyle Button component

--- components/navigation/Button.jsx
+++ components/navigation/Button.jsx
@@ -1,6 +1,6 @@
 const Button = ({ text }) => {
   return (
-    <div className="text-center py-3 px-5 border rounded-lg bg-white shadow-sm hover:shadow-lg hover:bg-gray-500 active:bg-gray-700 hover:text-white cursor-pointer active:shadow-lg transition-colors">
+    <div className="text-center py-3 px-5 rounded-lg bg-white shadow hover:shadow-lg hover:bg-gray-500 active:bg-gray-700 hover:text-white cursor-pointer active:shadow-lg transition-colors">
       {text}
     </div>
   );
