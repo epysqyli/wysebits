70d4565c2ed9e9014674c1bb43c49c6b7bbc5966
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-12-11 15:13:39 +0100

    Add upvote/downvote related api calls to EntryLogged components

--- components/books/tile-entry-components/EntryLogged.jsx
+++ components/books/tile-entry-components/EntryLogged.jsx
@@ -7,7 +7,14 @@ const EntryLogged = ({
   addInsightToState,
   removeInsightFromState,
   userId,
+  upvotedEntries,
+  downvotedEntries,
+  removeUpEntryFromState,
+  addUpEntryToState,
+  removeDownEntryFromState,
+  addDownEntryToState,
 }) => {
+  // methods related to like/heart functionalities
   const isFavInsight = () => {
     return insights.some((insight) => insight.id === data.id);
   };
@@ -30,6 +37,55 @@ const EntryLogged = ({
         { withCredentials: true }
       )
       .then((res) => removeInsightFromState(data))
+      .catch((err) => console.log(err));
+  };
+
+  // methods related to upvote and downvote functionalities
+  const isUpvoted = () => {
+    return upvotedEntries.some((entry) => entry.id == data.id);
+  };
+
+  const addToUpvoted = () => {
+    axios
+      .post(
+        `http://localhost:3001/api/users/${userId}/tile_entries/${data.id}/upvote`,
+        {},
+        { withCredentials: true }
+      )
+      .then((res) => addUpEntryToState(data))
+      .catch((err) => console.log(err));
+  };
+
+  const removeFromUpvoted = () => {
+    axios
+      .post(
+        `http://localhost:3001/api/users/${userId}/tile_entries/${data.id}/remove_upvote`,
+        {},
+        { withCredentials: true }
+      )
+      .then((res) => removeUpEntryFromState(data))
+      .catch((err) => console.log(err));
+  };
+
+  const addToDownvoted = () => {
+    axios
+      .post(
+        `http://localhost:3001/api/users/${userId}/tile_entries/${data.id}/downvote`,
+        {},
+        { withCredentials: true }
+      )
+      .then((res) => addDownEntryToState(data))
+      .catch((err) => console.log(err));
+  };
+
+  const removeFromDownvoted = () => {
+    axios
+      .post(
+        `http://localhost:3001/api/users/${userId}/tile_entries/${data.id}/remove_downvote`,
+        {},
+        { withCredentials: true }
+      )
+      .then((res) => removeDownEntryFromState(data))
       .catch((err) => console.log(err));
   };
 
