4abcc6ced826afa15f903504eec0d42643b70e38
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-12-16 19:23:37 +0100

    Fix unique prop warning for LatestBooks component

--- components/creators/LatestBooks.jsx
+++ components/creators/LatestBooks.jsx
@@ -11,14 +11,14 @@ const LatestBooks = ({ books }) => {
 
   return (
     <div>
-      <div className="text-center text-gray-800 text-3xl">
-        Latest books
-      </div>
+      <div className="text-center text-gray-800 text-3xl">Latest books</div>
       <div className="mx-5 my-10">
         {latest.map((book) => (
-          <Link href={`/books/${slug(book.title, book.id)}/1`}>
-            <div className="cursor-pointer shadow-md rounded my-5 bg-gray-100  mx-10">
-              <BookCard bookData={book} key={book.id} />
+          <Link href={`/books/${slug(book.title, book.id)}/1`} key={book.id}>
+            <div
+              className="cursor-pointer shadow-md rounded my-5 bg-gray-100  mx-10"
+            >
+              <BookCard bookData={book} />
             </div>
           </Link>
         ))}
