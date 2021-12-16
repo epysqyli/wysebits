3aa1882c3cfbfa07cd6aa275ee98305140c9a6c8
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-12-16 17:28:19 +0100

    Map books in LatestBooks component

--- components/creators/LatestBooks.jsx
+++ components/creators/LatestBooks.jsx
@@ -5,10 +5,17 @@ const LatestBooks = ({ books }) => {
   const [latest, setLatest] = useState(books.slice(0, 3));
 
   return (
-    <div className="mx-5 my-10">
-      {latest.map((book) => (
-        <BookCard bookData={book} key={book.id} />
-      ))}
+    <div>
+      <div className="text-center mt-10 text-gray-800 text-3xl">
+        Latest contributed books
+      </div>
+      <div className="mx-5 my-10">
+        {latest.map((book) => (
+          <div className="shadow-md rounded my-5 bg-gray-100  mx-10">
+            <BookCard bookData={book} key={book.id} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
