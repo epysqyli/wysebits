4e0ef688bce611bcadf6b7d9342868acd291615d
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-12-21 23:32:51 +0100

    Make footer responsive

--- components/layout/Footer.jsx
+++ components/layout/Footer.jsx
@@ -3,7 +3,7 @@ import Link from "next/dist/client/link";
 const Footer = () => {
   return (
     <footer>
-      <div className="h-60 mx-auto bg-bottom-home bg-cover bg-center">
+      <div className="h-60 lg:h-48 xl:h-40 mx-auto bg-bottom-home bg-cover bg-center">
         <div className="bg-gray-800 h-full bg-opacity-80 relative">
           <div className="w-4/6 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
             <div className="text-3xl font-bold text-gray-200 text-center">
