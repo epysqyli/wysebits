cae84c22536bf01c8fc2529b5e656239de0f4d8b
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-12-22 22:34:36 +0100

    Make CardBcgActions component responsive up to md

--- components/books/CardBcgActions.jsx
+++ components/books/CardBcgActions.jsx
@@ -2,13 +2,13 @@ import BookCard from "./BookCard";
 
 const CardBcgActions = ({ bookData, showEditForm, bcgImage }) => {
   return (
-    <div className="relative py-6">
+    <div className="relative py-6 md:py">
       <img
         src={bcgImage()}
         className="absolute top-0 left-1/2 -translate-x-1/2 blur-sm backdrop-brightness-50 grayscale-50 contrast-50 max-h-full w-full object-cover"
       />
 
-      <div className="w-4/5 mx-auto shadow-md backdrop-blur-md backdrop-brightness-75 text-white my-10 rounded-md relative z-10">
+      <div className="w-4/5 md:w-4/6 lg:w-3/6 mx-auto shadow-md backdrop-blur-md backdrop-brightness-75 text-white my-10 rounded-md relative z-10">
         <div className="mb-2">
           <BookCard bookData={bookData} />
         </div>
