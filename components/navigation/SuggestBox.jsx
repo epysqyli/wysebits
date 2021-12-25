7e90f6ea46300cbdaece2b2296f2e46d65dc73d6
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-12-25 18:45:02 +0100

    Remove code duplication in SuggestBox

--- components/navigation/SuggestBox.jsx
+++ components/navigation/SuggestBox.jsx
@@ -6,41 +6,28 @@ const SuggestBox = ({ suggestions, suggestLink }) => {
   const slug = (title, id) =>
     slugify(`${title}-${id}`, { lower: true, strict: true });
 
+  const buildLink = (result) => {
+    if (suggestLink == "/books/")
+      return `${suggestLink}${slug(result._source.title, result._source.id)}/1`;
+    else if (suggestLink == "/users/book-tiles/create/")
+      return `${suggestLink}${result._source.id}`;
+  };
+
   if (suggestions != null) {
-    if (suggestLink == "/books/") {
-      return (
-        <div>
-          {suggestions.map((s) => {
-            return (
-              <Link
-                key={s._source.id}
-                href={`${suggestLink}${slug(s._source.title, s._source.id)}/1`}
-              >
-                <div className="flex items-center justify-between px-2 bg-white my-1 shadow rounded-md text-gray-700">
-                  <div className="my-2">{s._source.title}</div>
-                  <ArrowUpRight size={18} color="gray" />
-                </div>
-              </Link>
-            );
-          })}
-        </div>
-      );
-    } else if (suggestLink == "/users/book-tiles/create/") {
-      return (
-        <div>
-          {suggestions.map((s) => {
-            return (
-              <Link key={s._source.id} href={`${suggestLink}${s._source.id}`}>
-                <div className="flex items-center justify-between px-2 bg-white my-1 shadow rounded-md text-gray-700">
-                  <div className="my-2">{s._source.title}</div>
-                  <ArrowUpRight size={18} color="gray" />
-                </div>
-              </Link>
-            );
-          })}
-        </div>
-      );
-    }
+    return (
+      <div>
+        {suggestions.map((s) => {
+          return (
+            <Link key={s._source.id} href={buildLink(s)}>
+              <div className="flex items-center justify-between px-2 bg-white my-1 shadow rounded-md text-gray-700">
+                <div className="my-2">{s._source.title}</div>
+                <ArrowUpRight size={18} color="gray" />
+              </div>
+            </Link>
+          );
+        })}
+      </div>
+    );
   } else {
     return null;
   }
