853c88113d50d5ab40aa69b57cd9a818fe65e5e9
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-12-25 17:11:11 +0100

    Add link to suggestions in SearchInput

--- components/navigation/SuggestBox.jsx
+++ components/navigation/SuggestBox.jsx
@@ -1,11 +1,26 @@
 import { ArrowUpRight } from "react-feather";
+import Link from "next/link";
+import slugify from "slugify";
 
 const SuggestBox = ({ suggestions }) => {
+  const slug = (title, id) =>
+    slugify(`${title}-${id}`, { lower: true, strict: true });
+
   if (suggestions != null) {
     return (
       <div>
         {suggestions.results.map((s) => {
-          return <div key={s._id}>{s._source.title}</div>;
+          return (
+            <Link
+              key={s._source.id}
+              href={`/books/${slug(s._source.title, s._source.id)}/1`}
+            >
+              <div className="flex items-center justify-between border px-2">
+                <div className="my-2">{s._source.title}</div>
+                <ArrowUpRight size={18} color="gray" />
+              </div>
+            </Link>
+          );
         })}
       </div>
     );
