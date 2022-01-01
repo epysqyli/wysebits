df54080dd0e95376eb7ebef3e9613324272f57d4
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-01-01 17:26:38 +0100

    Use next/image in CardBcgActions component

--- components/books/CardBcgActions.jsx
+++ components/books/CardBcgActions.jsx
@@ -1,11 +1,14 @@
 import BookCard from "./BookCard";
+import Image from "next/dist/client/image";
 
 const CardBcgActions = ({ bookData, showEditForm, bcgImage }) => {
   return (
     <div className="relative py-6 md:py">
-      <img
+      <Image
         src={bcgImage()}
-        className="absolute top-0 left-1/2 -translate-x-1/2 blur-sm backdrop-brightness-50 grayscale-50 contrast-50 max-h-full w-full object-cover"
+        className="absolute blur backdrop-brightness-50 grayscale-25 contrast-50"
+        layout="fill"
+        objectFit="cover"
       />
 
       <div className="w-4/5 md:w-4/6 lg:w-3/6 mx-auto shadow-md backdrop-blur-md backdrop-brightness-75 text-white my-10 rounded-md relative z-10">
