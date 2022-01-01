1b464dbd3a7f746660fae6686236257caaa87277
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-01-02 00:48:52 +0100

    Style users/book-search

--- pages/users/book-search/index.jsx
+++ pages/users/book-search/index.jsx
@@ -16,8 +16,8 @@ const BookSearch = ({ userState }) => {
           text="What book have you just read?"
         />
 
-        <div className="w-4/5 mx-auto md:w-4/6 lg:w-3/6">
-          <div className="mt-20">
+        <div className="w-4/5 mx-auto md:w-4/6 lg:w-3/6 xl:w-4/5 xl:flex xl:mt-20 xl:justify-around xl:mb-20">
+          <div className="mt-20 xl:mt-0 xl:w-2/4">
             <SearchInput
               pageDest="/users/book-search/"
               placeholder="Enter book title"
@@ -26,7 +26,7 @@ const BookSearch = ({ userState }) => {
             />
           </div>
 
-          <div className="mt-60 border-l-2 border-gray-400 pl-5">
+          <div className="mt-60 mb-20 xl:mb-0 xl:mt-0 border-l-2 pl-5 xl:w-2/5">
             <div className="text-lg">
               "Books are the treasured wealth of the world and the fit
               inheritance of generations and nations."
