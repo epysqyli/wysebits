d8199ddd04ce5bd8bbaf8dcc7289ebf9541527f3
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-01-01 22:39:13 +0100

    Adjust index page for xl breakpoints

--- pages/index.jsx
+++ pages/index.jsx
@@ -38,7 +38,7 @@ const Home = ({ categories, userState }) => {
       </div>
 
       <div className="xl:flex">
-        <div className="mb-10 w-4/5 mx-auto md:w-4/6 lg:w-3/6 xl:w-2/6 px-2 py-3 border-b-2">
+        <div className="mb-10 w-4/5 mx-auto md:w-4/6 lg:w-3/6 xl:w-2/6 px-2 py-3 xl:py-0 border-b-2">
           <div className="flex justify-center items-center gap-x-5">
             <Book size={36} strokeWidth={1.5} />
             <div className="text-4xl text-gray-700">Explore books</div>
@@ -56,7 +56,7 @@ const Home = ({ categories, userState }) => {
         <div className="mb-10 w-4/5 mx-auto md:w-4/6 lg:w-3/6 border-b-2 pb-10">
           <div className="flex justify-center items-center gap-x-5 mb-10">
             <Box size={36} strokeWidth={1.5} />
-            <div className="text-3xl text-gray-700">Explore categories</div>
+            <div className="text-3xl xl:text-4xl text-gray-700">Explore categories</div>
           </div>
           <div className="grid grid-cols-2 md:grid-cols-3 gap-x-4 gap-y-4">
             {categories.map((category) => {
