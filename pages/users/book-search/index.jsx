34a8fba82e751992f52942501bbd45f9e8ab0a75
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-01-02 00:39:53 +0100

    Small changes to users/book-search

--- pages/users/book-search/index.jsx
+++ pages/users/book-search/index.jsx
@@ -6,7 +6,7 @@ import NoAccess from "../../../components/users/NoAccess";
 const BookSearch = ({ userState }) => {
   if (userState.isLogged) {
     return (
-      <div className="pb-20">
+      <div>
         <Head>
           <title>Create book tile</title>
         </Head>
@@ -16,7 +16,7 @@ const BookSearch = ({ userState }) => {
           text="What book have you just read?"
         />
 
-        <div className="w-4/5 mx-auto md:w-4/6 lg:w-3/6 xl:w-2/6">
+        <div className="w-4/5 mx-auto md:w-4/6 lg:w-3/6">
           <div className="mt-20">
             <SearchInput
               pageDest="/users/book-search/"
@@ -26,12 +26,12 @@ const BookSearch = ({ userState }) => {
             />
           </div>
 
-          <div className="mt-60 border-l-2 border-gray-400 pl-3">
-            <div className="text-lg my-3">
+          <div className="mt-60 border-l-2 border-gray-400 pl-5">
+            <div className="text-lg">
               "Books are the treasured wealth of the world and the fit
               inheritance of generations and nations."
             </div>
-            <div className="text text-gray-600 italic text-right">
+            <div className="text mt-5 pr-10 text-gray-600 italic text-right">
               Henry David Thoreau, Walden
             </div>
           </div>
