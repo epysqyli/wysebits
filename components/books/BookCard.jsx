b30431e65e1bd273e3cd42b7f82bbe0d8c0e27a6
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-01-01 18:17:00 +0100

    Add style changes to BookCard layout and image

--- components/books/BookCard.jsx
+++ components/books/BookCard.jsx
@@ -6,22 +6,25 @@ const BookCard = ({ bookData }) => {
 
   return (
     <div className="flex justify-center gap-x-5 px-1 py-5">
-      <Image
-        className="w-2/6 rounded-md bg-gray-300 object-cover animate-show-up-slow shadow-lg"
-        src={dbSrc || olSrc || ""}
-        width="130"
-        height="200"
-        layout="intrinsic"
-        placeholder="blur"
-        blurDataURL="/images/book-placeholder.png"
-      />
-
-      <div className="w-3/6">
-        <div className="text-xl text-center mb-5 font-medium">
-          {bookData.title}
+      <div className="w-2/6">
+        <div className="shadow-md rounded-md">
+          <Image
+            src={dbSrc || olSrc || ""}
+            className="animate-show-up-slow rounded-md"
+            layout="responsive"
+            width="10"
+            height="15"
+            objectFit="cover"
+            placeholder="blur"
+            blurDataURL="/images/book-placeholder.png"
+          />
         </div>
-        <div className="text-sm text-center">{bookData.category.name}</div>
-        <div className="text-sm text-center italic my-5">
+      </div>
+
+      <div className="w-3/6 flex flex-col justify-around text-right">
+        <div className="text-xl mb-5 font-medium">{bookData.title}</div>
+        <div className="text-sm">{bookData.category.name}</div>
+        <div className="text-sm italic my-5">
           {bookData.authors[0]
             ? bookData.authors[0].full_name
             : "No authors found"}
