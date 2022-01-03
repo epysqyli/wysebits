17662a4c45cfdcbe284cea967101e76fd3e76d33
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-01-03 17:59:17 +0100

    Add conditional render in books/search/keywords/page for null searchResults

--- pages/books/search/[keywords]/[page].jsx
+++ pages/books/search/[keywords]/[page].jsx
@@ -19,8 +19,8 @@ export const getServerSideProps = async (context) => {
 
   return {
     props: {
-      searchResults: searchResults.data.results,
-      pageNum: searchResults.data.page_num,
+      searchResults: searchResults.data.results || null,
+      pageNum: searchResults.data.page_num || null,
       keywords: keywords,
     },
   };
@@ -42,51 +42,57 @@ const BookSearchResults = ({ searchResults, keywords, pageNum }) => {
     setTimeout(showBtn, 2000);
   }, []);
 
-  return (
-    <div>
-      <div className="my-10 w-4/5 mx-auto md:w-4/6 lg:w-3/6 xl:w-2/6">
-        <SearchInput
-          pageDest="/books/search/"
-          placeholder="Search for any book"
-        />
-      </div>
-      <div className="mt-10 w-11/12 grid gap-y-12 md:grid-cols-2 md:gap-x-6 lg:grid-cols-3 xl:gap-x-10 2xl:grid-cols-4 mx-auto">
-        {searchResults.length != 0
-          ? searchResults.map((book) => {
-              return (
-                <BookSearchTile
-                  bookData={book}
-                  destPage={`/books/${slug(
-                    book._source.title,
-                    book._source.id
-                  )}/1`}
-                  key={book._id}
-                />
-              );
-            })
-          : null}
-
-        {btnVisible ? <CreateBookBtn /> : null}
-      </div>
-
-      <div className="flex items-center my-16 w-4/5 mx-auto gap-x-4">
-        <div className="w-1/2">
-          <NavButtonElastic
-            btnText="Previous page"
-            clientUrl={clientUrl}
-            pageNum={pageNum - 1}
+  if (searchResults !== null) {
+    return (
+      <div>
+        <div className="my-10 w-4/5 mx-auto md:w-4/6 lg:w-3/6 xl:w-2/6">
+          <SearchInput
+            pageDest="/books/search/"
+            placeholder="Search for any book"
           />
         </div>
-        <div className="w-1/2">
-          <NavButtonElastic
-            btnText="Next page"
-            clientUrl={clientUrl}
-            pageNum={pageNum + 1}
-          />
+        <div className="mt-10 w-11/12 grid gap-y-12 md:grid-cols-2 md:gap-x-6 lg:grid-cols-3 xl:gap-x-10 2xl:grid-cols-4 mx-auto">
+          {searchResults.length != 0
+            ? searchResults.map((book) => {
+                return (
+                  <BookSearchTile
+                    bookData={book}
+                    destPage={`/books/${slug(
+                      book._source.title,
+                      book._source.id
+                    )}/1`}
+                    key={book._id}
+                  />
+                );
+              })
+            : null}
+
+          {btnVisible ? <CreateBookBtn /> : null}
+        </div>
+
+        <div className="flex items-center my-16 w-4/5 mx-auto gap-x-4">
+          <div className="w-1/2">
+            <NavButtonElastic
+              btnText="Previous page"
+              clientUrl={clientUrl}
+              pageNum={pageNum - 1}
+            />
+          </div>
+          <div className="w-1/2">
+            <NavButtonElastic
+              btnText="Next page"
+              clientUrl={clientUrl}
+              pageNum={pageNum + 1}
+            />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
+
+  if (searchResults === null) {
+    return <div>Nothing to see here</div>;
+  }
 };
 
 export default BookSearchResults;
