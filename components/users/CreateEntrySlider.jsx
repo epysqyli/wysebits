1e8ac2133b4e92676e7ca0532d05aaa29e30d361
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-01-05 16:55:48 +0100

    Add functions to increment/decrement current counter on CreateEntrySlider

--- components/users/CreateEntrySlider.jsx
+++ components/users/CreateEntrySlider.jsx
@@ -9,19 +9,35 @@ const CreateEntrySlider = ({
   saveForLater,
 }) => {
   const [current, setCurrent] = useState(0);
-  const increment = () => setCurrent(current++);
-  const decrement = () => setCurrent(current--);
+  const increment = () => setCurrent(handleIncrement(current));
+  const decrement = () => setCurrent(handleDecrement(current));
+
+  const handleIncrement = (value) => {
+    if (value < entries.length - 1) return value + 1;
+    return 0;
+  };
+
+  const handleDecrement = (value) => {
+    if (value === 0) return entries.length - 1;
+    return value;
+  };
 
   return (
-    <div className="flex justify-around items-center">
-      <ChevronLeft  />
-      <CreateEntryForm
-        entry={entries[current]}
-        isEntryValid={isEntryValid}
-        handleChange={handleChange}
-        saveForLater={saveForLater}
-      />
-      <ChevronRight />
+    <div className="flex justify-between items-center gap-x-3">
+      <div onClick={decrement}>
+        <ChevronLeft />
+      </div>
+      <div className="flex-grow">
+        <CreateEntryForm
+          entry={entries[current]}
+          isEntryValid={isEntryValid}
+          handleChange={handleChange}
+          saveForLater={saveForLater}
+        />
+      </div>
+      <div onClick={increment}>
+        <ChevronRight />
+      </div>
     </div>
   );
 };
