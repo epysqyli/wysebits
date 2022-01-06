e3fee8c30225e912eacf725576066bb6af6cf9f6
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-01-06 12:05:40 +0100

    Add basic display logic to manage-categories

--- pages/users/actions/manage-profile/manage-categories.jsx
+++ pages/users/actions/manage-profile/manage-categories.jsx
@@ -19,6 +19,11 @@ export const getServerSideProps = async (context) => {
 };
 
 const ManageCategories = ({ categories, favCategories }) => {
+  const isFav = (cat) => favCategories.some((userCat) => userCat.id === cat.id);
+
+  const baseClass = "border rounded py-1 text-center";
+  const favClass = "border rounded py-1 text-center bg-white";
+  
   return (
     <div>
       <div className="bg-categories bg-cover bg-center shadow">
@@ -27,6 +32,19 @@ const ManageCategories = ({ categories, favCategories }) => {
           <div className="text-sm mt-5 font-normal">
             This will influence the suggestions that you get on the feed
           </div>
+        </div>
+      </div>
+
+      <div className="mx-auto w-4/5 py-10">
+        <div className="text-center text-lg mb-10">Choose up to three categories</div>
+        <div className="grid grid-cols-2 gap-px">
+          {categories.map((cat) => {
+            return (
+              <div key={cat.id} className={isFav(cat) ? favClass : baseClass}>
+                {cat.name}
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
