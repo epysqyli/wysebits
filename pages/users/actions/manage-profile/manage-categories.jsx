17fd72c0f0eec087338eccaba0fd8a27ec9150c8
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-01-06 11:53:54 +0100

    Add header to manage-categories

--- pages/users/actions/manage-profile/manage-categories.jsx
+++ pages/users/actions/manage-profile/manage-categories.jsx
@@ -18,8 +18,19 @@ export const getServerSideProps = async (context) => {
   };
 };
 
-const ManageCategories = ({categories, favCategories}) => {
-  return <div></div>;
+const ManageCategories = ({ categories, favCategories }) => {
+  return (
+    <div>
+      <div className="bg-categories bg-cover bg-center shadow">
+        <div className="bg-gray-800 bg-opacity-70 text-white text-2xl font-medium text-center py-12">
+          <div>Update your favorite categories</div>
+          <div className="text-sm mt-5 font-normal">
+            This will influence the suggestions that you get on the feed
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ManageCategories;
