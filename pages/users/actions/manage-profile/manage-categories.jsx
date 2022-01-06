7d38eb7171d07698df2ac948ef199fc53b16a9b2
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-01-06 15:02:20 +0100

    Make manage-categories work

--- pages/users/actions/manage-profile/manage-categories.jsx
+++ pages/users/actions/manage-profile/manage-categories.jsx
@@ -4,6 +4,7 @@ import {
   getCategories,
 } from "../../../../lib/serverSideMethods";
 
+import axios from "axios";
 import { useState } from "react";
 
 export const getServerSideProps = async (context) => {
@@ -20,15 +21,47 @@ export const getServerSideProps = async (context) => {
   };
 };
 
-const ManageCategories = ({ cats, favCats }) => {
+const ManageCategories = ({ cats, favCats, userState }) => {
   const [categories, setCategories] = useState(cats);
   const [favCategories, setFavCategories] = useState(favCats);
 
   const isFav = (cat) => favCategories.some((userCat) => userCat.id === cat.id);
+  const isAddable = () => (favCategories.length == 3 ? false : true);
 
-  const baseClass = "border rounded py-1 text-center bg-gray-200";
-  const favClass = "border rounded py-1 text-center bg-white";
-  
+  const addToFav = async (cat) => {
+    await axios({
+      method: "post",
+      url: `http://localhost:3001/api/users/${userState.user.id}/categories/${cat.id}/add_to_fav`,
+      withCredentials: true,
+    });
+    addToState(cat);
+  };
+
+  const removeFromFav = async (cat) => {
+    await axios({
+      method: "post",
+      url: `http://localhost:3001/api/users/${userState.user.id}/categories/${cat.id}/remove_from_fav`,
+      withCredentials: true,
+    });
+    removeFromState(cat);
+  };
+
+  const addToState = (cat) => {
+    const newFavs = [...favCategories, cat];
+    setFavCategories(newFavs);
+  };
+
+  const removeFromState = (cat) => {
+    const newFavs = favCategories.filter((item) => item.id !== cat.id);
+    setFavCategories(newFavs);
+  };
+
+  const baseClass =
+    "rounded py-1 text-center text-gray-500 hover:text-gray-900 bg-gray-100 shadow cursor-pointer transition-all";
+  const favClass =
+    "rounded py-1 text-center bg-white shadow-inner cursor-pointer transition-all";
+  const noSelection = baseClass + " cursor-default";
+
   return (
     <div>
       <div className="bg-categories bg-cover bg-center shadow">
@@ -41,11 +74,23 @@ const ManageCategories = ({ cats, favCats }) => {
       </div>
 
       <div className="mx-auto w-4/5 py-10">
-        <div className="text-center text-lg mb-10">Choose up to three categories</div>
-        <div className="grid grid-cols-2 gap-px">
+        <div className="text-center text-lg mb-10">
+          Choose up to three categories
+        </div>
+        <div className="grid grid-cols-2 gap-y-2 gap-x-2">
           {categories.map((cat) => {
             return (
-              <div key={cat.id} className={isFav(cat) ? favClass : baseClass}>
+              <div
+                key={cat.id}
+                className={
+                  isFav(cat) ? favClass : !isAddable() ? noSelection : baseClass
+                }
+                onClick={() => {
+                  isFav(cat)
+                    ? removeFromFav(cat)
+                    : isAddable() && addToFav(cat);
+                }}
+              >
                 {cat.name}
               </div>
             );
