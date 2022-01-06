3837ec3417f8136ae145730921e829510513ee13
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-01-06 19:46:43 +0100

    Change text size in feed page

--- pages/feed/[page].jsx
+++ pages/feed/[page].jsx
@@ -13,7 +13,7 @@ const Feed = ({ entries, pagy, userState }) => {
   return (
     <div>
       <div className="bg-feed bg-cover bg-center shadow">
-        <div className="bg-gray-800 bg-opacity-70 text-white text-3xl font-medium text-center py-12">
+        <div className="bg-gray-800 bg-opacity-70 text-white text-2xl md:text-3xl font-medium text-center py-12">
           Latest insights from all creators
         </div>
       </div>
