0b93cdecbda7488f2a12da9555609fd7ed2393e3
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-01-07 17:15:31 +0100

    Update creators/username layout for latestentries

--- components/creators/LatestBooks.jsx
+++ components/creators/LatestBooks.jsx
@@ -12,7 +12,7 @@ const LatestBooks = ({ books }) => {
   return (
     <div>
       <div className="text-center text-gray-800 text-3xl">Latest books</div>
-      <div className="mt-10 w-11/12 grid gap-y-12 md:grid-cols-2 md:gap-x-6 lg:grid-cols-3 xl:gap-x-10 2xl:grid-cols-4 mx-auto">
+      <div className="mt-10 w-11/12 grid gap-y-12 md:grid-cols-2 md:gap-x-6 xl:gap-x-10 xl:grid-cols-2 xl:w-4/5 mx-auto">
         {latest.map((book) => (
           <Link href={`/books/${slug(book.title, book.id)}/1`} key={book.id}>
             <div className="cursor-pointer shadow-md rounded bg-gray-100 hover:bg-gray-200 active:bg-gray-300 transition-colors">
