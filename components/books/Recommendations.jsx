1ba0bfb5b1356e330064927280989b1407aad2e3
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-01-08 18:54:59 +0100

    Change recommendations from border to shadow

--- components/books/Recommendations.jsx
+++ components/books/Recommendations.jsx
@@ -7,7 +7,7 @@ const Recommendations = ({ recommendations }) => {
     <>
       {recommendations.map((book) => (
         <Link href={`/books/${slug(book.title, book.id)}/1`}>
-          <div className="border rounded cursor-pointer hover:bg-gray-200 active:bg-gray-300 active:shadow-inner">
+          <div className="shadow-md rounded cursor-pointer hover:bg-gray-200 active:bg-gray-300 active:shadow-inner">
             <BookCard bookData={book} />
           </div>
         </Link>
