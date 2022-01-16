87f5105385891755e8bc1d726942d69e41a9b1c8
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-01-16 19:52:48 +0100

    Remove nested if in SuggestBox

--- components/navigation/SuggestBox.jsx
+++ components/navigation/SuggestBox.jsx
@@ -20,67 +20,65 @@ const SuggestBox = ({ suggestions, suggestLink, searchMode }) => {
       return `${suggestLink}${result._source.id}`;
   };
 
-  if (suggestions !== null) {
-    if (searchMode === "books") {
-      return (
-        <div>
-          {suggestions.map((s) => {
-            return (
-              <Link key={s._source.id} href={buildLink(s)}>
-                <div className="flex items-center justify-between px-2 md:px-10 bg-white my-1 shadow rounded-md text-gray-800 cursor-pointer group hover:scale-105 transition-transform active:bg-gray-100">
-                  <div className="my-3 w-4/5">
-                    <div>{s._source.title}</div>
-                    <div className="flex gap-x-2 text-sm text-gray-600">
-                      <div>
-                        {s._source.authors
-                          ? s._source.authors[0]
-                            ? s._source.authors[0].full_name
-                            : "No authors"
-                          : "No Authors"}
-                      </div>
-                      <div>|</div>
-                      <div>
-                        {s._source.category
-                          ? s._source.category.name
-                          : "No Category"}
-                      </div>
+  if (suggestions !== null && searchMode === "books") {
+    return (
+      <div>
+        {suggestions.map((s) => {
+          return (
+            <Link key={s._source.id} href={buildLink(s)}>
+              <div className="flex items-center justify-between px-2 md:px-10 bg-white my-1 shadow rounded-md text-gray-800 cursor-pointer group hover:scale-105 transition-transform active:bg-gray-100">
+                <div className="my-3 w-4/5">
+                  <div>{s._source.title}</div>
+                  <div className="flex gap-x-2 text-sm text-gray-600">
+                    <div>
+                      {s._source.authors
+                        ? s._source.authors[0]
+                          ? s._source.authors[0].full_name
+                          : "No authors"
+                        : "No Authors"}
+                    </div>
+                    <div>|</div>
+                    <div>
+                      {s._source.category
+                        ? s._source.category.name
+                        : "No Category"}
                     </div>
                   </div>
-                  <ArrowUpRight
-                    size={18}
-                    color="gray"
-                    className="group-hover:scale-125 transition-transform"
-                  />
                 </div>
-              </Link>
-            );
-          })}
-        </div>
-      );
-    }
+                <ArrowUpRight
+                  size={18}
+                  color="gray"
+                  className="group-hover:scale-125 transition-transform"
+                />
+              </div>
+            </Link>
+          );
+        })}
+      </div>
+    );
+  }
 
-    if (searchMode === "authors") {
-      return (
-        <div>
-          {suggestions.map((s) => {
-            return (
-              <Link key={s._source.id} href={buildLink(s)}>
-                <div className="flex items-center justify-between px-2 md:px-10 bg-white my-1 shadow rounded-md text-gray-800 cursor-pointer group hover:scale-105 transition-transform active:bg-gray-100">
-                  <div className="my-3 w-4/5">
-                    <div>{s._source.full_name}</div>
-                  </div>
-                  <ArrowUpRight
-                    size={18}
-                    color="gray"
-                    className="group-hover:scale-125 transition-transform"
-                  />
+  if (suggestions !== null && searchMode === "authors") {
+    return (
+      <div>
+        {suggestions.map((s) => {
+          return (
+            <Link key={s._source.id} href={buildLink(s)}>
+              <div className="flex items-center justify-between px-2 md:px-10 bg-white my-1 shadow rounded-md text-gray-800 cursor-pointer group hover:scale-105 transition-transform active:bg-gray-100">
+                <div className="my-3 w-4/5">
+                  <div>{s._source.full_name}</div>
                 </div>
-              </Link>
-            );
-          })}
-        </div>
-      );
-    }
+                <ArrowUpRight
+                  size={18}
+                  color="gray"
+                  className="group-hover:scale-125 transition-transform"
+                />
+              </div>
+            </Link>
+          );
+        })}
+      </div>
+    );
   }
 
   return null;
