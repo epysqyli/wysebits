09750d7e83bce9f9d05f5ad9aa5c1f9aa908a3c4
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-01-17 00:34:48 +0100

    Add pages to author search

--- pages/authors/search/[keywords]/[page].jsx
+++ pages/authors/search/[keywords]/[page].jsx
@@ -1,7 +1,9 @@
 import { searchAuthors } from "../../../../lib/serverSideMethods";
 import AuthorResult from "../../../../components/authors/AuthorResult";
+import NavButtonElastic from "../../../../components/navigation/NavButtonElastic";
 
 export const getServerSideProps = async (context) => {
+  const keywords = context.query.keywords;
   const splitKeywords = context.query.keywords.split("-").join(" ");
   const pageNum = context.query.page;
 
@@ -11,16 +13,37 @@ export const getServerSideProps = async (context) => {
     props: {
       searchResults: searchResults.data.results || null,
       pageNum: searchResults.data.page_num || null,
+      keywords: keywords,
     },
   };
 };
 
-const Author = ({ searchResults, pageNum }) => {
+const Author = ({ searchResults, pageNum, keywords }) => {
+  const clientUrl = `/authors/search/${keywords}`;
+
   return (
     <div>
-      {searchResults.map((author) => {
-        return <AuthorResult author={author._source} key={author._id} />;
-      })}
+      <div className="mt-10 w-11/12 grid gap-y-12 md:grid-cols-2 md:gap-x-6 lg:grid-cols-3 xl:gap-x-10 2xl:grid-cols-4 mx-auto">
+        {searchResults.map((author) => {
+          return <AuthorResult author={author._source} key={author._id} />;
+        })}
+      </div>
+      <div className="flex justify-around my-16 lg:my-32 md:w-4/5 lg:w-1/2 mx-auto">
+        <div className="w-1/3">
+          <NavButtonElastic
+            btnText="Previous page"
+            clientUrl={clientUrl}
+            pageNum={pageNum - 1}
+          />
+        </div>
+        <div className="w-1/3">
+          <NavButtonElastic
+            btnText="Next page"
+            clientUrl={clientUrl}
+            pageNum={pageNum + 1}
+          />
+        </div>
+      </div>
     </div>
   );
 };
