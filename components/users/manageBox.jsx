eb4049f37cfc1da049702cadfdd8b3bdbae7c311
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-01-18 23:15:14 +0100

    Increase gap-x in manageBox component

--- components/users/manageBox.jsx
+++ components/users/manageBox.jsx
@@ -4,7 +4,7 @@ import Link from "next/dist/client/link";
 const ManageBox = ({ text, href }) => {
   return (
     <Link href={href}>
-      <div className="flex items-center justify-between bg-white shadow px-5 py-2 rounded group cursor-pointer">
+      <div className="flex items-center justify-center gap-x-10 md:gap-x-20 bg-white shadow py-2 rounded group cursor-pointer">
         <ChevronRight className="group-hover:scale-110 group-active:scale-100 transition-transform" />
         <div className="text-gray-800">{text}</div>
       </div>
