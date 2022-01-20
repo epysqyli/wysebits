75309154e2c5f8ccf3b1fad82f3c9cc3adcf3fac
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-01-20 16:07:42 +0100

    Remove scale on hover

--- components/users/UserAction.jsx
+++ components/users/UserAction.jsx
@@ -6,9 +6,9 @@ const UserAction = ({ href, text, bcgImg }) => {
       <div
         className={`h-28 ${bcgImg} bg-cover bg-center rounded-md cursor-pointer shadow-md hover:shadow-lg group animate-show-up`}
       >
-        <div className="bg-gray-800 h-full bg-opacity-70 relative rounded group-hover:bg-opacity-75 transition active:bg-gray-900">
+        <div className="bg-gray-800 h-full bg-opacity-70 relative rounded-md group-hover:bg-opacity-75 transition active:bg-gray-900">
           <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-            <div className="text-white text-2xl text-center group-hover:scale-110 transition">
+            <div className="text-white text-2xl text-center transition">
               {text}
             </div>
           </div>
