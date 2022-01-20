4f9c0378048a204002cc9dd3834fbf44abd85aca
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-01-20 22:21:26 +0100

    Setup avatar change

--- pages/users/actions/manage-profile/manage-picture.jsx
+++ pages/users/actions/manage-profile/manage-picture.jsx
@@ -5,16 +5,41 @@ import Image from "next/dist/client/image";
 
 const ManagePicture = ({ userState, userLoading }) => {
   const [avatarUrl, setAvatarUrl] = useState(null);
+  const [file, setFile] = useState(null);
 
-  const updateImage = async () => {
+  const updateImage = async (userId, formData) => {
     return await axios({
-      method: "post",
-      url: `http://localhost:3001/api/users/${userState.user.id}/update_avatar`,
+      method: "POST",
+      url: `http://localhost:3001/api/users/${userId}/update_avatar`,
+      data: formData,
       withCredentials: true,
     });
   };
 
-  useEffect(() => setAvatarUrl(userState.user.avatar || null));
+  const isBtnActive = () => {
+    if (avatarUrl !== userState.user.avatar) return true;
+    return false;
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData();
+    if (file) formData.append("user[avatar]", file);
+
+    await updateImage(userState.user.id, formData);
+  };
+
+  useEffect(() => setAvatarUrl(userState.user.avatar || null), []);
+
+  useEffect(() => {
+    if (file === null) return;
+
+    const fileUrl = URL.createObjectURL(file);
+    setAvatarUrl(fileUrl);
+
+    return () => URL.revokeObjectURL(fileUrl);
+  }, [file]);
 
   return (
     <div>
@@ -24,17 +49,17 @@ const ManagePicture = ({ userState, userLoading }) => {
       </div>
 
       <div className="w-4/5 mx-auto my-10">
-        <div className="text-center mb-20 text-2xl border-b pb-2">
+        <div className="text-center mb-20 text-2xl border-b pb-2 text-gray-700">
           Current profile picture
         </div>
 
         <div className="flex items-center justify-around">
           <div>
-            {userState.user.avatar === null || userLoading === true ? (
+            {avatarUrl === null || userLoading === true ? (
               <ImageLoader size={60} strokeWidth={1.5} color="gray" />
             ) : (
               <Image
-                src={userState.user.avatar}
+                src={avatarUrl}
                 width={125}
                 height={125}
                 className="rounded-md"
@@ -43,15 +68,38 @@ const ManagePicture = ({ userState, userLoading }) => {
           </div>
         </div>
 
-        <div className="text-center my-20">
+        <form className="text-center my-20" onSubmit={handleSubmit}>
           <label
             htmlFor="new-image"
             className="cursor-pointer border p-4 rounded-md shadow hover:shadow-md transition-shadow text-gray-800"
           >
             Upload a new avatar for your profile
           </label>
-          <input type="file" name="" id="new-image" className="hidden" />
-        </div>
+          <input
+            type="file"
+            name=""
+            id="new-image"
+            className="hidden"
+            onChange={(e) => setFile(e.target.files[0])}
+          />
+
+          {isBtnActive() ? (
+            <button
+              className="mt-20 mx-auto px-4 py-2 rounded-md bg-gray-500 text-white hover:shadow-md active:bg-gray-700"
+              type="submit"
+            >
+              Change picture!
+            </button>
+          ) : (
+            <button
+              className="mt-20 mx-auto px-4 py-2 rounded-md text-gray-300 border cursor-default"
+              type="submit"
+              disabled
+            >
+              Change picture!
+            </button>
+          )}
+        </form>
       </div>
     </div>
   );
