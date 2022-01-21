17fef1c68f5c8caf7f77a1b73c1b313eb5a5cad7
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-01-21 20:02:53 +0100

    Remove cursor pointer where not needed

--- pages/users/actions/followers/[page].jsx
+++ pages/users/actions/followers/[page].jsx
@@ -45,7 +45,7 @@ const Followers = ({ followers, userState, pagy, unpagedFollowers }) => {
         <WelcomeTop text="Users following you" bcgImg="bg-followers" />
         <div className="mx-auto w-4/5 md:w-4/6 lg:w-3/6 xl:w-2/6 2xl:w-1/3 py-20 lg:py-40 2xl:py-48">
           <NoItem message="You have no followers yet!" />
-          <div className="border px-5 pt-5 mt-20 lg:mt-32 bg-gray-100 rounded-md shadow group transition-all cursor-pointer hover:shadow-md">
+          <div className="border px-5 pt-5 mt-20 lg:mt-32 bg-gray-100 rounded-md shadow group transition-all hover:shadow-md">
             <div>
               Keep sharing your knowledge for the books you've read in order to
               provide the community with your insights!
--- pages/users/book-tiles/[page].jsx
+++ pages/users/book-tiles/[page].jsx
@@ -42,7 +42,7 @@ const UserBookTiles = ({ bookTiles, pagy, userState }) => {
         />
         <div className="mx-auto w-4/5 md:w-4/6 lg:w-3/6 xl:w-2/6 2xl:w-1/3 py-20 lg:py-40 2xl:py-48">
           <NoItem message="You have no contributions yet" />
-          <div className="border px-5 pt-3 md:px-8 md:pt-5 mt-20 lg:mt-32 bg-gray-100 rounded-md shadow group transition-all cursor-pointer hover:shadow-md text-center">
+          <div className="border px-5 pt-3 md:px-8 md:pt-5 mt-20 lg:mt-32 bg-gray-100 rounded-md shadow group transition-all hover:shadow-md text-center">
             <div>
               Start contributing now by choosing the first book for which you
               want to add your own personal insights
