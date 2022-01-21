3d09679ef77e4db5875a1a151fa2b7a835506566
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-01-21 20:29:59 +0100

    Remove useless hover state animations

--- components/users/UserAction.jsx
+++ components/users/UserAction.jsx
@@ -4,11 +4,11 @@ const UserAction = ({ href, text, bcgImg }) => {
   return (
     <Link href={href}>
       <div
-        className={`h-28 ${bcgImg} bg-cover bg-center rounded-md cursor-pointer shadow-md hover:shadow-lg group animate-show-up hover:bg-top transition-all`}
+        className={`h-28 ${bcgImg} bg-cover bg-center rounded-md cursor-pointer shadow-md hover:shadow-lg group animate-show-up transition-all`}
       >
-        <div className="bg-gray-800 h-full bg-opacity-70 relative rounded-md group-hover:bg-opacity-75 transition active:bg-gray-900">
+        <div className="bg-gray-800 h-full bg-opacity-70 relative rounded-md group-hover:bg-opacity-80 transition active:bg-gray-900">
           <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-            <div className="text-white text-2xl text-center transition">
+            <div className="text-white text-2xl text-center transition-all">
               {text}
             </div>
           </div>
