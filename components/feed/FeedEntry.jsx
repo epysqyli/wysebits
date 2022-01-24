90f8e14d56a7aafe1e50dfd266f31609a2d77620
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-01-24 19:01:51 +0100

    Make feed entry content tiles wider

--- components/feed/FeedEntry.jsx
+++ components/feed/FeedEntry.jsx
@@ -14,7 +14,7 @@ const FeedEntry = ({
   setFollowedUsers,
 }) => {
   return (
-    <div className="my-4 bg-white md:w-4/5 mx-auto lg:flex xl:w-4/6 2xl:w-1/2 animate-show-up-slow shadow">
+    <div className="py-4 md:py-0 md:my-4 bg-white md:w-4/5 mx-auto lg:flex xl:w-4/6 2xl:w-1/2 animate-show-up-slow md:shadow">
       <div className="w-4/5 md:w-3/5 mx-auto lg:w-1/3 lg:bg-gray-100">
         <BookCard
           bookData={entry.book_tile.book}
