b778c75dadff64291bf9bf5b904ba5eac5aeffe5
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-01-26 00:36:06 +0100

    Refactor PageNavButton

--- components/navigation/PageNavButton.jsx
+++ components/navigation/PageNavButton.jsx
@@ -3,21 +3,20 @@ import Link from "next/dist/client/link";
 const PageNavButton = ({ btnText, url, clientUrl }) => {
   const pageNum = url.split("?page=")[1] || "";
 
-  if (pageNum == "") {
+  if (pageNum == "")
     return (
       <div className="py-2 text-center text-gray-300 rounded shadow">
         {btnText}
       </div>
     );
-  } else {
-    return (
-      <Link href={`${clientUrl}/${pageNum}`}>
-        <div className="py-2 text-center rounded border shadow cursor-pointer hover:bg-gray-200 active:bg-gray-300 active:text-white transition-colors">
-          {btnText}
-        </div>
-      </Link>
-    );
-  }
+
+  return (
+    <Link href={`${clientUrl}/${pageNum}`}>
+      <div className="py-2 text-center rounded border shadow cursor-pointer hover:bg-gray-200 active:bg-gray-300 active:text-white transition-colors">
+        {btnText}
+      </div>
+    </Link>
+  );
 };
 
 export default PageNavButton;
