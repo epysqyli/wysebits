c2b4a6ee897e5e419317f2951d16d3b500c18ce6
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-01-25 23:02:19 +0100

    Add tentative border-yellow

--- components/navigation/CategoryButton.jsx
+++ components/navigation/CategoryButton.jsx
@@ -3,7 +3,7 @@ import Link from "next/dist/client/link";
 const CategoryButton = ({ category }) => {
   return (
     <Link href={`/categories/${category.slug}/1`}>
-      <div className="text-gray-800 text-center p-2 rounded-md shadow cursor-pointer bg-white hover:bg-gray-100 hover:shadow-md active:bg-gray-200 active:shadow-inner transition-all">
+      <div className="text-gray-800 text-center px-4 py-4 rounded-md shadow cursor-pointer bg-white hover:bg-gray-100 hover:shadow-md active:bg-gray-200 border-transparent border-2 hover:border-yellow-500 active:shadow-inner transition-all">
         {category.name}
       </div>
     </Link>
