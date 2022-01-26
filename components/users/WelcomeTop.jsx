cb6917a3bfb666728f48f033b7772aa2134adf8c
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-01-26 17:20:43 +0100

    Increase WelcomeTop height

--- components/users/WelcomeTop.jsx
+++ components/users/WelcomeTop.jsx
@@ -1,11 +1,11 @@
 const WelcomeTop = ({ text, bcgImg }) => {
   return (
     <div
-      className={`h-36 ${bcgImg} bg-cover bg-center shadow-md hover:shadow-lg group animate-show-up`}
+      className={`h-36 md:h-44 ${bcgImg} bg-cover bg-center shadow-md hover:shadow-lg group animate-show-up`}
     >
       <div className="bg-gray-800 h-full bg-opacity-70 relative group-hover:bg-opacity-75 transition active:bg-gray-900">
         <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-          <div className="text-white text-2xl font-bold text-center group-hover:scale-105 transition">
+          <div className="text-white text-2xl md:text-3xl font-bold text-center group-hover:scale-105 transition">
             {text}
           </div>
         </div>
