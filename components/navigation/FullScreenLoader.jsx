021ec584bef43bffc78e1378caec8e83f57d78cf
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-01-27 15:09:28 +0100

    Decrease loader size

--- components/navigation/FullScreenLoader.jsx
+++ components/navigation/FullScreenLoader.jsx
@@ -5,7 +5,7 @@ const FullScreenLoader = () => {
     <div className="fixed w-full h-screen z-30 bg-gray-200 bg-opacity-50">
       <div className="w-min animate-spin mt-10 mr-10 ml-auto">
         <Loader
-          size={52}
+          size={40}
           color="black"
           className="absolute -translate-x-1/2 -translate-y-1/2 border rounded-full p-2 shadow bg-white"
         />
