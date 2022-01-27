43304167ae606bdbb4f3a9796ec7fc27e1b00361
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-01-28 00:16:44 +0100

    Change Recommendations hover state

--- components/books/Recommendations.jsx
+++ components/books/Recommendations.jsx
@@ -7,7 +7,7 @@ const Recommendations = ({ recommendations }) => {
     <>
       {recommendations.map((book) => (
         <Link href={`/books/${slug(book.title, book.id)}/1`} key={book.id}>
-          <div className="shadow-md rounded-md cursor-pointer bg-gray-50 hover:bg-gray-200 active:bg-gray-300 active:shadow-inner">
+          <div className="shadow-md rounded-md cursor-pointer bg-gray-50 hover:bg-gray-100 active:bg-gray-200 active:shadow-inner">
             <BookCard bookData={book} />
           </div>
         </Link>
