74e657145b694ee3f48f87c783ebe7616eae0117
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-01-28 14:46:31 +0100

    Apply white bg to EntryGuestTitle

--- components/books/tile-entry-components/EntryGuestTitle.jsx
+++ components/books/tile-entry-components/EntryGuestTitle.jsx
@@ -5,19 +5,22 @@ import { slug, shortenText } from "../../../lib/utils";
 
 const EntryGuestTitle = ({ entryProp }) => {
   return (
-    <div className="flex flex-col justify-around h-full">
-      <div className="bg-white text-justify leading-6 rounded-md hover:shadow-md transition-all">
+    <div className="flex flex-col justify-around h-full bg-white rounded-md shadow hover:shadow-md transition-all">
+      <div className="text-justify leading-6">
         <Link
           href={`/books/${slug(
             entryProp.book_tile.book.title,
             entryProp.book_tile.book.id
           )}/1`}
         >
-          <div className="px-2 pt-4 flex justify-center items-center gap-x-5 hover:scale-105 active:text-black transition-transform cursor-pointer">
+          <div className="px-2 pt-4 flex justify-center items-center gap-x-5 transition-transform cursor-pointer group">
             <div className="text-sm text-gray-600">
               {shortenText(entryProp.book_tile.book.title, 5)}
             </div>
-            <ArrowUpRight size={18} className="text-gray-600" />
+            <ArrowUpRight
+              size={18}
+              className="text-gray-600 group-hover:scale-125 active:text-black transition-transform"
+            />
           </div>
         </Link>
       </div>
