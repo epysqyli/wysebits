859dad18560b3bbdcb9df4dd338fdcf21f85e9df
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-01-29 18:02:57 +0100

    Apply latest book tile style to creators page

--- components/creators/LatestBooks.jsx
+++ components/creators/LatestBooks.jsx
@@ -15,7 +15,7 @@ const LatestBooks = ({ books }) => {
       <div className="my-10 pb-10 w-11/12 grid gap-y-12 md:grid-cols-2 md:gap-x-6 xl:gap-x-10 xl:grid-cols-2 xl:w-4/5 2xl:w-3/5 mx-auto border-b-2">
         {latest.map((book) => (
           <Link href={`/books/${slug(book.title, book.id)}/1`} key={book.id}>
-            <div className="rounded-md bg-white shadow-lg hover:bg-gray-50 hover:shadow-xl transition-all cursor-pointer active:shadow-inner">
+            <div className="rounded-md bg-white shadow-lg hover:bg-gray-50 hover:shadow-xl transition-all cursor-pointer active:shadow-inner border-b-2 border-blue-200 hover:border-blue-300">
               <BookCard bookData={book} />
             </div>
           </Link>
