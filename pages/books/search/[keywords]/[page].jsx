434db96585f8a02701c77a27433a8ea77d1a7f40
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-01-31 22:33:42 +0100

    Apply linear gradient to glabals

--- pages/books/search/[keywords]/[page].jsx
+++ pages/books/search/[keywords]/[page].jsx
@@ -39,7 +39,7 @@ const BookSearchResults = ({ searchResults, keywords, pagy }) => {
 
   if (searchResults.length !== 0) {
     return (
-      <div className="pb-52 md:pb-80 lg:pb-72 2xl:pb-80 bg-gradient-to-br from-blue-50 via-gray-100 to-gray-200">
+      <div>
         <div className="py-10 w-4/5 mx-auto md:w-4/6 lg:w-3/6 xl:w-2/6">
           <SearchInput
             pageDest="/books/search/"
