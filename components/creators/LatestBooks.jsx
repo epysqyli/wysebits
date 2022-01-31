371df6558d7dd71a07dd9e076e7bccefbaace06a
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-01-31 10:49:41 +0100

    Adjust creators index latestbooks width

--- components/creators/LatestBooks.jsx
+++ components/creators/LatestBooks.jsx
@@ -12,7 +12,7 @@ const LatestBooks = ({ books }) => {
   return (
     <div>
       <div className="text-center text-gray-800 text-5xl">Latest books</div>
-      <div className="my-10 pb-10 w-11/12 grid gap-y-12 md:grid-cols-2 md:gap-x-6 xl:gap-x-10 xl:grid-cols-2 xl:w-4/5 2xl:w-3/5 mx-auto border-b-2">
+      <div className="my-10 pb-10 w-11/12 grid gap-y-12 md:grid-cols-2 md:gap-x-6 xl:gap-x-10 xl:grid-cols-2 lg:w-4/5 xl:w-11/12 2xl:w-3/5 mx-auto border-b-2">
         {latest.map((book) => (
           <Link href={`/books/${slug(book.title, book.id)}/1`} key={book.id}>
             <div className="rounded-md bg-white shadow-lg hover:bg-gray-50 hover:shadow-xl transition-all cursor-pointer active:shadow-inner border-b-2 border-blue-200 hover:border-blue-300">
