b5039bf4fc15251efe93f3b45684eec677835a7a
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-01-31 15:27:26 +0100

    Add styles for matching and unmatching passwords

--- pages/registrations/reset_password/[token].jsx
+++ pages/registrations/reset_password/[token].jsx
@@ -1,20 +1,51 @@
 import axios from "axios";
 import { Key } from "react-feather";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export const getServerSideProps = (context) => {
   return { props: { token: context.query.token } };
 };
 
 const ResetPassword = ({ token }) => {
+  const [psws, setPsws] = useState({
+    password: "",
+    passwordConfirmation: "",
+  });
+
+  const handleChange = (e) => {
+    setPsws({
+      ...psws,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const isConfirmed = () => {
+    if (psws.password !== "" && psws.password === psws.passwordConfirmation) {
+      console.log(true);
+      return true;
+    }
+
+    console.log(false);
+    return false;
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  };
+
+  useEffect(() => isConfirmed(), [psws.passwordConfirmation]);
+
   return (
     <div className="py-10">
       <div className="flex justify-between items-center w-5/6 md:w-4/6 lg:w-3/6 mx-auto mt-5 pb-5 border-b-2">
-        <Key size={36} />
+        <Key size={36} className="text-gray-700" />
         <div className="text-3xl text-gray-800">Choose a new password</div>
       </div>
 
-      <form className="mx-auto md:w-4/6 lg:w-3/6 xl:w-2/5 py-10 md:border md:shadow rounded">
+      <form
+        className="mx-auto md:w-4/6 lg:w-3/6 xl:w-2/5 py-10 md:border md:shadow rounded"
+        onSubmit={handleSubmit}
+      >
         <div className="w-4/6 mx-auto my-10">
           <label htmlFor="password" className="pl-1 text-xl text-gray-800">
             New password
@@ -24,6 +55,7 @@ const ResetPassword = ({ token }) => {
             name="password"
             id="password"
             className="block mt-4 w-full border-none focus:ring-blue-400 ring-0 focus:ring-2 rounded-lg shadow-sm focus:shadow-md"
+            onChange={handleChange}
             required
           />
         </div>
@@ -37,9 +69,16 @@ const ResetPassword = ({ token }) => {
           </label>
           <input
             type="password"
-            name="password-confirmation"
+            name="passwordConfirmation"
             id="password-confirmation"
-            className="block mt-4 w-full border-none focus:ring-blue-400 ring-0 focus:ring-2 rounded-lg shadow-sm focus:shadow-md"
+            className={`block mt-4 w-full border-none focus:ring-blue-400 ring-0 focus:ring-2 rounded-lg shadow-sm focus:shadow-md ${
+              isConfirmed()
+                ? "bg-green-100"
+                : psws.passwordConfirmation !== ""
+                ? "bg-red-100"
+                : null
+            }`}
+            onChange={handleChange}
             required
           />
         </div>
