a85b9264200718930f373320e53cebcc4b509824
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-02-02 12:05:06 +0100

    Add image to relatedUser component

--- components/users/RelatedUser.jsx
+++ components/users/RelatedUser.jsx
@@ -7,6 +7,7 @@ import {
 } from "react-feather";
 import axios from "axios";
 import Link from "next/dist/client/link";
+import Image from "next/dist/client/image";
 import { countTotalInsights } from "../../lib/creatorMethods";
 import { addFollowedUserToState } from "../../lib/tileEntryMethods";
 import { removeFollowedUserFromState } from "../../lib/tileEntryMethods";
@@ -51,14 +52,22 @@ const RelatedUser = ({
   return (
     <Link href={`/creators/${relatedUser.username}`}>
       <div className="flex justify-around items-center gap-x-10">
-        <div>
+        {relatedUser.avatar_url !== null ? (
+          <Image
+            src={relatedUser.avatar_url}
+            className="animate-show-up-slow rounded-full"
+            layout="fixed"
+            width="60"
+            height="60"
+          />
+        ) : (
           <User
             size={60}
             strokeWidth={1.5}
             color="gray"
             className="bg-gray-300 rounded-full p-2"
           />
-        </div>
+        )}
         <div>
           <div className="flex items-center justify-between mb-5">
             <div className="text-2xl font-bold text-gray-700">
