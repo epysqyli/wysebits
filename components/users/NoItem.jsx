9fb6d21c8221570ad82c3b848651fd268d57c9ac
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-02-21 20:30:47 +0100

    actions: use uniform style for all actions

--- components/users/NoItem.jsx
+++ components/users/NoItem.jsx
@@ -3,14 +3,14 @@ import { AlertCircle } from "react-feather";
 const NoItem = ({ message }) => {
   return (
     <div>
-      <div className="flex justify-center items-center gap-x-5">
+      <div className="flex justify-center items-center">
         <AlertCircle
           size={36}
           strokeWidth={1.5}
           className="rounded-3xl w-1/6"
           fill="lightgray"
         />
-        <div className="text w-4/6 md:text-lg">{message}</div>
+        <div className="text w-4/6 md:text-lg text-center">{message}</div>
       </div>
     </div>
   );
--- pages/users/actions/stats.jsx
+++ pages/users/actions/stats.jsx
@@ -18,157 +18,153 @@ export const getServerSideProps = async (context) => {
 const Stats = ({ entries }) => {
   if (entries.best_net_entry !== null)
     return (
-      <div className="w-5/6 md:w-4/6 lg:w-11/12 xl:w-5/6 mx-auto py-10">
-        <div className="flex justify-between items-center md:justify-around md:border-b-2 md:border-gray-300 md:pb-10">
-          <Activity size={36} className="text-gray-800" />
-          <div className="text-gray-800 text-2xl">
-            Some stats for your profile
-          </div>
-        </div>
-
-        <div className="mx-auto my-20 grid grid-cols-1 lg:grid-cols-3 lg:gap-x-5 gap-y-10">
-          <div className="bg-gradient-to-br from-blue-50 to-blue-100 py-5 rounded text-gray-800 shadow hover:shadow-md transition-shadow border-2 border-white">
-            <div className="flex items-start justify-around border-b pb-4">
-              <Award size={50} strokeWidth={1.25} className="text-gray-700" />
-              <div>
-                <div className="text-xl p-2 bg-gray-50 rounded-md shadow">
-                  Best net entry
-                </div>
-                <div className="my-2 text-center">
-                  Total net votes: {entries.best_net_entry.net_votes}
+      <div>
+        <WelcomeTop text="Here are some stats for you" bcgImg="bg-stats" />
+        <div className="w-5/6 md:w-4/6 lg:w-11/12 xl:w-5/6 mx-auto py-10">
+          <div className="mx-auto my-20 grid grid-cols-1 lg:grid-cols-3 lg:gap-x-5 gap-y-10">
+            <div className="bg-gradient-to-br from-blue-50 to-blue-100 py-5 rounded text-gray-800 shadow hover:shadow-md transition-shadow border-2 border-white">
+              <div className="flex items-start justify-around border-b pb-4">
+                <Award size={50} strokeWidth={1.25} className="text-gray-700" />
+                <div>
+                  <div className="text-xl p-2 bg-gray-50 rounded-md shadow">
+                    Best net entry
+                  </div>
+                  <div className="my-2 text-center">
+                    Total net votes: {entries.best_net_entry.net_votes}
+                  </div>
                 </div>
               </div>
+
+              <div className="text-center my-8">
+                <Link
+                  href={`/books/${slug(
+                    entries.best_net_entry.book_tile.book.title,
+                    entries.best_net_entry.book_tile.book.id
+                  )}/1`}
+                >
+                  <div className="my-2 cursor-pointer">
+                    {entries.best_net_entry.book_tile.book.title}
+                  </div>
+                </Link>
+                <Link
+                  href={`/authors/${slug(
+                    entries.best_net_entry.book_tile.book.authors[0].full_name,
+                    entries.best_net_entry.book_tile.book.authors[0].id
+                  )}/1`}
+                >
+                  <div className="my-2 text-sm italic cursor-pointer hover:text-black">
+                    {entries.best_net_entry.book_tile.book.authors[0].full_name}
+                  </div>
+                </Link>
+                <Link
+                  href={`/categories/${entries.best_net_entry.book_tile.book.category.slug}/1`}
+                >
+                  <div className="my-2 text-sm italic cursor-pointer hover:text-black">
+                    {entries.best_net_entry.book_tile.book.category.name}
+                  </div>
+                </Link>
+              </div>
+
+              <p className="mt-5 py-3 px-5 text-justify font-light text-black">
+                {shortenText(entries.best_net_entry.content, 50)}
+              </p>
             </div>
 
-            <div className="text-center my-8">
-              <Link
-                href={`/books/${slug(
-                  entries.best_net_entry.book_tile.book.title,
-                  entries.best_net_entry.book_tile.book.id
-                )}/1`}
-              >
-                <div className="my-2 cursor-pointer">
-                  {entries.best_net_entry.book_tile.book.title}
-                </div>
-              </Link>
-              <Link
-                href={`/authors/${slug(
-                  entries.best_net_entry.book_tile.book.authors[0].full_name,
-                  entries.best_net_entry.book_tile.book.authors[0].id
-                )}/1`}
-              >
-                <div className="my-2 text-sm italic cursor-pointer hover:text-black">
-                  {entries.best_net_entry.book_tile.book.authors[0].full_name}
-                </div>
-              </Link>
-              <Link
-                href={`/categories/${entries.best_net_entry.book_tile.book.category.slug}/1`}
-              >
-                <div className="my-2 text-sm italic cursor-pointer hover:text-black">
-                  {entries.best_net_entry.book_tile.book.category.name}
-                </div>
-              </Link>
-            </div>
-
-            <p className="mt-5 py-3 px-5 text-justify font-light text-black">
-              {shortenText(entries.best_net_entry.content, 50)}
-            </p>
-          </div>
-
-          <div className="bg-gradient-to-br from-green-50 to-green-100 py-5 rounded text-gray-800 shadow hover:shadow-md transition-shadow border-2 border-white">
-            <div className="flex items-start justify-around border-b pb-4">
-              <Smile size={50} strokeWidth={1.25} className="text-gray-700" />
-              <div>
-                <div className="text-xl p-2 bg-gray-50 rounded-md shadow">
-                  Most upvoted
-                </div>
-                <div className="my-2 text-center">
-                  Total upvotes: {entries.most_upvoted.upvotes}
+            <div className="bg-gradient-to-br from-green-50 to-green-100 py-5 rounded text-gray-800 shadow hover:shadow-md transition-shadow border-2 border-white">
+              <div className="flex items-start justify-around border-b pb-4">
+                <Smile size={50} strokeWidth={1.25} className="text-gray-700" />
+                <div>
+                  <div className="text-xl p-2 bg-gray-50 rounded-md shadow">
+                    Most upvoted
+                  </div>
+                  <div className="my-2 text-center">
+                    Total upvotes: {entries.most_upvoted.upvotes}
+                  </div>
                 </div>
               </div>
+
+              <div className="text-center my-8">
+                <Link
+                  href={`/books/${slug(
+                    entries.most_upvoted.book_tile.book.title,
+                    entries.most_upvoted.book_tile.book.id
+                  )}/1`}
+                >
+                  <div className="my-2 cursor-pointer">
+                    {entries.most_upvoted.book_tile.book.title}
+                  </div>
+                </Link>
+                <Link
+                  href={`/authors/${slug(
+                    entries.most_upvoted.book_tile.book.authors[0].full_name,
+                    entries.most_upvoted.book_tile.book.authors[0].id
+                  )}/1`}
+                >
+                  <div className="my-2 text-sm italic cursor-pointer hover:text-black">
+                    {entries.most_upvoted.book_tile.book.authors[0].full_name}
+                  </div>
+                </Link>
+                <Link
+                  href={`/categories/${entries.most_upvoted.book_tile.book.category.slug}/1`}
+                >
+                  <div className="my-2 text-sm italic cursor-pointer hover:text-black">
+                    {entries.most_upvoted.book_tile.book.category.name}
+                  </div>
+                </Link>
+              </div>
+
+              <p className="mt-5 py-3 px-5 text-justify font-light text-black">
+                {shortenText(entries.most_upvoted.content, 50)}
+              </p>
             </div>
 
-            <div className="text-center my-8">
-              <Link
-                href={`/books/${slug(
-                  entries.most_upvoted.book_tile.book.title,
-                  entries.most_upvoted.book_tile.book.id
-                )}/1`}
-              >
-                <div className="my-2 cursor-pointer">
-                  {entries.most_upvoted.book_tile.book.title}
-                </div>
-              </Link>
-              <Link
-                href={`/authors/${slug(
-                  entries.most_upvoted.book_tile.book.authors[0].full_name,
-                  entries.most_upvoted.book_tile.book.authors[0].id
-                )}/1`}
-              >
-                <div className="my-2 text-sm italic cursor-pointer hover:text-black">
-                  {entries.most_upvoted.book_tile.book.authors[0].full_name}
-                </div>
-              </Link>
-              <Link
-                href={`/categories/${entries.most_upvoted.book_tile.book.category.slug}/1`}
-              >
-                <div className="my-2 text-sm italic cursor-pointer hover:text-black">
-                  {entries.most_upvoted.book_tile.book.category.name}
-                </div>
-              </Link>
-            </div>
-
-            <p className="mt-5 py-3 px-5 text-justify font-light text-black">
-              {shortenText(entries.most_upvoted.content, 50)}
-            </p>
-          </div>
-
-          <div className="bg-gradient-to-br from-red-50 to-red-100 py-5 rounded text-gray-800 shadow hover:shadow-md transition-shadow border-2 border-white">
-            <div className="flex items-start justify-around border-b pb-4">
-              <Frown size={50} strokeWidth={1.25} className="text-gray-700" />
-              <div>
-                <div className="text-xl p-2 bg-gray-50 rounded-md shadow">
-                  Most downvoted
-                </div>
-                <div className="my-2 text-center">
-                  Total downvotes: {entries.most_downvoted.downvotes}
+            <div className="bg-gradient-to-br from-red-50 to-red-100 py-5 rounded text-gray-800 shadow hover:shadow-md transition-shadow border-2 border-white">
+              <div className="flex items-start justify-around border-b pb-4">
+                <Frown size={50} strokeWidth={1.25} className="text-gray-700" />
+                <div>
+                  <div className="text-xl p-2 bg-gray-50 rounded-md shadow">
+                    Most downvoted
+                  </div>
+                  <div className="my-2 text-center">
+                    Total downvotes: {entries.most_downvoted.downvotes}
+                  </div>
                 </div>
               </div>
-            </div>
 
-            <div className="text-center my-8">
-              <Link
-                href={`/books/${slug(
-                  entries.most_downvoted.book_tile.book.title,
-                  entries.most_downvoted.book_tile.book.id
-                )}/1`}
-              >
-                <div className="my-2 cursor-pointer">
-                  {entries.most_downvoted.book_tile.book.title}
-                </div>
-              </Link>
-              <Link
-                href={`/authors/${slug(
-                  entries.most_downvoted.book_tile.book.authors[0].full_name,
-                  entries.most_downvoted.book_tile.book.authors[0].id
-                )}/1`}
-              >
-                <div className="my-2 text-sm italic cursor-pointer hover:text-black">
-                  {entries.most_downvoted.book_tile.book.authors[0].full_name}
-                </div>
-              </Link>
-              <Link
-                href={`/categories/${entries.most_downvoted.book_tile.book.category.slug}/1`}
-              >
-                <div className="my-2 text-sm italic cursor-pointer hover:text-black">
-                  {entries.most_downvoted.book_tile.book.category.name}
-                </div>
-              </Link>
-            </div>
+              <div className="text-center my-8">
+                <Link
+                  href={`/books/${slug(
+                    entries.most_downvoted.book_tile.book.title,
+                    entries.most_downvoted.book_tile.book.id
+                  )}/1`}
+                >
+                  <div className="my-2 cursor-pointer">
+                    {entries.most_downvoted.book_tile.book.title}
+                  </div>
+                </Link>
+                <Link
+                  href={`/authors/${slug(
+                    entries.most_downvoted.book_tile.book.authors[0].full_name,
+                    entries.most_downvoted.book_tile.book.authors[0].id
+                  )}/1`}
+                >
+                  <div className="my-2 text-sm italic cursor-pointer hover:text-black">
+                    {entries.most_downvoted.book_tile.book.authors[0].full_name}
+                  </div>
+                </Link>
+                <Link
+                  href={`/categories/${entries.most_downvoted.book_tile.book.category.slug}/1`}
+                >
+                  <div className="my-2 text-sm italic cursor-pointer hover:text-black">
+                    {entries.most_downvoted.book_tile.book.category.name}
+                  </div>
+                </Link>
+              </div>
 
-            <p className="mt-5 py-3 px-5 text-justify font-light text-black">
-              {shortenText(entries.most_downvoted.content, 50)}
-            </p>
+              <p className="mt-5 py-3 px-5 text-justify font-light text-black">
+                {shortenText(entries.most_downvoted.content, 50)}
+              </p>
+            </div>
           </div>
         </div>
       </div>
@@ -177,8 +173,8 @@ const Stats = ({ entries }) => {
   return (
     <div>
       <WelcomeTop
-        text="Books for which you have shared insights"
-        bcgImg="bg-check-book-tiles"
+        text="Here are some stats for you"
+        bcgImg="bg-stats"
       />
       <div className="mx-auto w-4/5 md:w-4/6 lg:w-3/6 xl:w-2/6 2xl:w-1/3 py-20 lg:py-40 2xl:py-48">
         <NoItem message="You have no contributions yet" />
