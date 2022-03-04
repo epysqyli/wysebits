66d58d72d01654ecfcbb98577730a893de2557ca
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-03-04 19:29:50 +0100

    NoItem: change text alignment

--- components/users/NoItem.jsx
+++ components/users/NoItem.jsx
@@ -3,14 +3,14 @@ import { AlertCircle } from "react-feather";
 const NoItem = ({ message }) => {
   return (
     <div>
-      <div className="flex justify-center items-center">
+      <div className="flex justify-around items-center">
         <AlertCircle
           size={36}
           strokeWidth={1.5}
           className="rounded-3xl w-1/6"
           fill="lightgray"
         />
-        <div className="text w-4/6 md:text-lg text-center">{message}</div>
+        <div className="text w-4/6 md:text-lg text">{message}</div>
       </div>
     </div>
   );
