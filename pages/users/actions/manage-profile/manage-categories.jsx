8ddbc84235dbe55c652ace84175e4f3607d8e93d
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-03-06 17:08:21 +0100

    manage-categories: apply select-none

--- pages/users/actions/manage-profile/manage-categories.jsx
+++ pages/users/actions/manage-profile/manage-categories.jsx
@@ -58,7 +58,7 @@ const ManageCategories = ({ cats, favCats, userState }) => {
   };
 
   const baseClass =
-    "rounded py-2 text-center text-gray-400 hover:text-gray-900 bg-gray-100 cursor-pointer border-2 transition-all";
+    "rounded py-2 text-center text-gray-400 hover:text-gray-900 bg-gray-100 cursor-pointer border-2 transition-all select-none";
   const favClass =
     "rounded py-2 text-center bg-white shadow-inner cursor-pointer transition-all border-2 border-blue-400";
   const noSelection = baseClass + " cursor-default";
