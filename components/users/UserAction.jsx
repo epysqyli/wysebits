0565173fb1d2a47638d9054e1d99c1016069a280
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-03-14 22:49:26 +0100

    user actions: style actions

--- components/users/UserAction.jsx
+++ components/users/UserAction.jsx
@@ -3,12 +3,14 @@ import Link from "next/link";
 const UserAction = ({ href, text, bcgImg }) => {
   return (
     <Link href={href}>
-      <div className="border-2 rounded cursor-pointer group">
-        <div className="text-center text-lg tracking-tight py-1 bg-gray-300">{text}</div>
+      <div className="cursor-pointer group relative group">
         <div
           className={`h-28 xl:h-36 ${bcgImg} bg-cover bg-center rounded shadow-lg hover:shadow-lg group transition-all`}
         >
-          <div className="bg-gray-900 h-full bg-opacity-50 relative group-hover:opacity-25 transition-colors"></div>
+          <div className="absolute z-10 left-1/2 -translate-x-1/2 top-1 text-center text-md tracking-tight font-medium py-1 text-white mb-1 w-4/5 mx-auto uppercase border-b-2 group-hover:bg-gray-50 group-hover:text-gray-800 transition-all">
+            {text}
+          </div>
+          <div className="bg-gray-900 h-full bg-opacity-60 relative group-hover:opacity-25 transition-colors"></div>
         </div>
       </div>
     </Link>
