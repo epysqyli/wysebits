c06d1246490469ed26b0a1bd8c98cd9965e5f91f
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-03-15 22:28:01 +0100

    user actions index: refine style

--- components/users/UserAction.jsx
+++ components/users/UserAction.jsx
@@ -7,10 +7,10 @@ const UserAction = ({ href, text, bcgImg }) => {
         <div
           className={`h-28 xl:h-36 ${bcgImg} bg-cover bg-center rounded shadow-lg hover:shadow-lg group transition-all`}
         >
-          <div className="absolute z-10 left-1/2 -translate-x-1/2 top-1 text-center text-md tracking-tight font-medium py-1 text-white mb-1 w-4/5 mx-auto uppercase border-b-2 group-hover:bg-gray-50 group-hover:text-gray-800 transition-all">
+          <div className="absolute z-10 left-1/2 -translate-x-1/2 top-1 text-center text-md tracking-tight font-medium py-1 text-white mb-1 w-4/5 mx-auto uppercase border-b-2 group-hover:bg-gray-50 group-hover:text-gray-800">
             {text}
           </div>
-          <div className="bg-gray-900 h-full bg-opacity-60 relative group-hover:opacity-25 transition-colors"></div>
+          <div className="bg-gray-900 h-full bg-opacity-60 relative group-hover:opacity-25 rounded transition-all"></div>
         </div>
       </div>
     </Link>
--- pages/users/actions/index.jsx
+++ pages/users/actions/index.jsx
@@ -12,7 +12,7 @@ const Actions = ({ userState }) => {
         </Head>
 
         <div className="bg-user-welcome bg-cover bg-center shadow-xl lg:w-4/5 2xl:w-2/3 lg:mt-5 lg:rounded-md mx-auto">
-          <div className="bg-gray-800 bg-opacity-70 text-white text-2xl font-bold text-center py-12 lg:py-16 lg:rounded-md">
+          <div className="bg-gray-800 bg-opacity-70 text-white text-3xl font-medium text-center py-12 lg:py-16 lg:rounded-md">
             <div>Hi {userState.user.username}</div>
             <div className="mt-2">What are you up to today?</div>
           </div>
