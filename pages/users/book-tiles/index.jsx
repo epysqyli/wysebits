fbdcff9c3be585b1301e403463f8c984a148bf8b
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-03-26 00:01:57 +0100

    search: add to users book tiles

--- pages/users/book-tiles/index.jsx
+++ pages/users/book-tiles/index.jsx
@@ -5,22 +5,36 @@ import NoItem from "../../../components/users/NoItem";
 import SearchInput from "../../../components/navigation/SearchInput";
 import Pagination from "../../../components/navigation/Pagination";
 import { getLoggedUser, getBookTiles } from "../../../lib/serverSideMethods";
+import { searchWithinBookTiles } from "../../../lib/searchMethods";
+import SpecificSearch from "../../../components/search/SpecificSearch";
+import NoResults from "../../../components/search/NoResults";
 
 export const getServerSideProps = async (context) => {
   try {
     const pageNum = context.query.page;
 
     const loggedUser = await getLoggedUser(context);
-    const bookTiles = await getBookTiles(loggedUser, pageNum);
 
-    const nonEmptyBookTiles = bookTiles.data.tiles.filter(
-      (book_tile) => book_tile.tile_entries.length != 0
-    );
+    let bookTiles;
+    if (context.query.searchTerms === undefined) {
+      bookTiles = await getBookTiles(loggedUser, pageNum);
+    } else {
+      const keywords = context.query.searchTerms;
+      bookTiles = await searchWithinBookTiles(
+        loggedUser.data.user.id,
+        keywords,
+        pageNum
+      );
+    }
 
     return {
       props: {
-        bookTiles: nonEmptyBookTiles,
+        bookTiles: bookTiles.data.tiles,
         pagy: bookTiles.data.pagy,
+        currentSearchTerms: context.query.searchTerms ?? null,
+        searchParams: context.query.searchTerms
+          ? { searchTerms: context.query.searchTerms }
+          : null,
       },
     };
   } catch (error) {
@@ -30,8 +44,34 @@ export const getServerSideProps = async (context) => {
   }
 };
 
-const UserBookTiles = ({ bookTiles, pagy, userState }) => {
+const UserBookTiles = ({
+  bookTiles,
+  pagy,
+  userState,
+  currentSearchTerms,
+  searchParams,
+}) => {
   const clientUrl = "/users/book-tiles";
+
+  if (userState.isLogged && bookTiles.length === 0 && currentSearchTerms)
+    return (
+      <>
+        <WelcomeTop
+          text="Books for which you have shared insights"
+          bcgImg="bg-check-book-tiles"
+        />
+
+        <div className="mt-5">
+          <SpecificSearch
+            placeholder="search within books"
+            baseUrl={`/users/book-tiles`}
+            currentSearchTerms={currentSearchTerms}
+          />
+        </div>
+
+        <NoResults />
+      </>
+    );
 
   if (userState.isLogged && bookTiles.length === 0)
     return (
@@ -66,6 +106,15 @@ const UserBookTiles = ({ bookTiles, pagy, userState }) => {
           text="Books for which you have shared insights"
           bcgImg="bg-check-book-tiles"
         />
+
+        <div className="mt-5">
+          <SpecificSearch
+            placeholder="search within books"
+            baseUrl={`/users/book-tiles`}
+            currentSearchTerms={currentSearchTerms}
+          />
+        </div>
+
         <div className="py-10 w-11/12 lg:w-4/5 xl:w-11/12 grid gap-y-12 md:grid-cols-2 md:gap-x-6 xl:grid-cols-3 xl:gap-x-10 2xl:grid-cols-4 mx-auto">
           {bookTiles.map((bookTile) => {
             return (
@@ -80,7 +129,7 @@ const UserBookTiles = ({ bookTiles, pagy, userState }) => {
           })}
         </div>
 
-        <Pagination clientUrl={clientUrl} pagy={pagy} />
+        <Pagination clientUrl={clientUrl} pagy={pagy} opts={searchParams} />
       </>
     );
 
