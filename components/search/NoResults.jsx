69480bded754ede6b59bc8a1ae34234e4f198a0a
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-03-26 16:31:04 +0100

    no results component: increase width

--- components/search/NoResults.jsx
+++ components/search/NoResults.jsx
@@ -2,7 +2,7 @@ import { Info } from "react-feather";
 
 const NoResults = () => {
   return (
-    <div className="flex items-center justify-center gap-x-5 w-4/5 mx-auto mt-10">
+    <div className="flex items-center justify-center gap-x-5 w-11/12 mx-auto mt-10">
       <Info size={36} strokeWidth={1.5} className="text-gray-600" />
       <div className="text-lg">No results</div>
     </div>
