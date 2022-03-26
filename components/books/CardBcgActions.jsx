7af8a7789cc001017ad1fb182fcfcd154bb66967
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-03-26 11:51:22 +0100

    card actions: increase width for sm

--- components/books/CardBcgActions.jsx
+++ components/books/CardBcgActions.jsx
@@ -3,7 +3,7 @@ import Image from "next/dist/client/image";
 
 const CardBcgActions = ({ bookData, showEditForm, bcgImage }) => {
   return (
-    <div className="relative py-6 md:py lg:w-4/5 2xl:w-2/3 lg:mt-5 lg:rounded-md mx-auto">
+    <div className="relative py-6 lg:w-4/5 2xl:w-2/3 lg:mt-5 lg:rounded-md mx-auto">
       <Image
         src={bcgImage()}
         className="absolute blur backdrop-brightness-50 grayscale-25 contrast-50 lg:rounded-md"
@@ -11,7 +11,7 @@ const CardBcgActions = ({ bookData, showEditForm, bcgImage }) => {
         objectFit="cover"
       />
 
-      <div className="w-4/5 md:w-4/6 lg:w-4/6 xl:w-3/5 2xl:w-1/2 mx-auto shadow-md backdrop-blur-md backdrop-brightness-75 text-white my-10 rounded-md relative z-10">
+      <div className="w-11/12 md:w-4/6 lg:w-4/6 xl:w-3/5 2xl:w-1/2 mx-auto shadow-md backdrop-blur-md backdrop-brightness-75 text-white rounded-md relative z-10">
         <div className="mb-2">
           <BookCard bookData={bookData} />
         </div>
