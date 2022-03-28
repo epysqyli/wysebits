f6dcc6607e12b82b7f60779ef9fe1bbc10aeb539
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-03-28 22:38:43 +0200

    restyle category button

--- components/navigation/CategoryButton.jsx
+++ components/navigation/CategoryButton.jsx
@@ -12,13 +12,10 @@ const CategoryButton = ({ category }) => {
         },
       }}
     >
-      <div className="text-gray-900 break-words text-lg tracking-tight px-4 pt-2 pb-6 rounded-md cursor-pointer border-blue-300 border-2 bg-white  hover:bg-yellow-50 hover:border-yellow-400 active:bg-blue-300 active:shadow-inner active:text-white shadow-md transition-all relative group">
-        <div>{category.name}</div>
-        <ArrowRightCircle
-          size={18}
-          strokeWidth={2}
-          className="absolute bottom-2 right-5 text-yellow-500 group-active:text-yellow-300 opacity-0 group-hover:opacity-75 group-hover:right-2 transition-all"
-        />
+      <div className="text-gray-900 text-xl tracking-tight h-16 rounded-md cursor-pointer border-blue-400 border-2 bg-white hover:bg-yellow-50 hover:border-yellow-400 active:bg-blue-300 active:shadow-inner active:text-white shadow-md transition-all relative group">
+        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full">
+          <div className="text-center">{category.name}</div>
+        </div>
       </div>
     </Link>
   );
