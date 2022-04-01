76f53ef60890b566af0afdd72ddf4e7d37be4f82
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-04-01 22:33:30 +0200

    entryLogged: DRY code for feed/normal entries

--- components/books/tile-entry-components/EntryLogged.jsx
+++ components/books/tile-entry-components/EntryLogged.jsx
@@ -31,7 +31,7 @@ const EntryLogged = ({
     return (
       <div className="flex flex-col justify-around h-full min-h-24rem">
         <div className="lg:border-b-2 lg:border-l-2 rounded-bl py-5 px-10 md:px-16 font-light whitespace-pre-line flex-grow mt-10 mx-auto text-justify md:text-left lg:mt-0 lg:w-full">
-          {entryProp.content}
+          {showComments ? <Comments comments={comments} /> : entryProp.content}
         </div>
 
         <EntryActions
