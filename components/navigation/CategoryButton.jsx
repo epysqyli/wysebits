ae0d2ea79658fff583fd083a1e42f5451441a1df
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-04-06 23:26:57 +0200

    category button: add left border

--- components/navigation/CategoryButton.jsx
+++ components/navigation/CategoryButton.jsx
@@ -11,7 +11,7 @@ const CategoryButton = ({ category }) => {
         },
       }}
     >
-      <div className="text-gray-900 text-xl tracking-tight h-16 rounded cursor-pointer bg-white hover:bg-yellow-50 active:bg-blue-400 active:shadow-inner active:text-white transition-all relative group">
+      <div className="text-gray-900 text-xl tracking-tight h-16 rounded-tr rounded-br cursor-pointer bg-white hover:bg-yellow-50 active:bg-blue-400 active:shadow-inner active:text-white transition-all relative group border-l-4 border-gray-300">
         <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-11/12">
           <div className="text-center">{category.name}</div>
         </div>
