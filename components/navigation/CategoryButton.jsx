3551fc0b3a501d6dfb13f6b6f67eebbaed32e415
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-04-07 20:35:50 +0200

    user action: add select-none

--- components/navigation/CategoryButton.jsx
+++ components/navigation/CategoryButton.jsx
@@ -11,7 +11,7 @@ const CategoryButton = ({ category }) => {
         },
       }}
     >
-      <div className="text-gray-900 text-xl tracking-tight h-16 rounded-tr rounded-br cursor-pointer bg-white hover:bg-yellow-50 active:bg-blue-400 active:shadow-inner active:text-white transition-all relative group border-l-4 border-gray-300">
+      <div className="text-gray-900 text-xl tracking-tight h-16 rounded-tr rounded-br cursor-pointer bg-white hover:bg-blue-50 active:bg-blue-400 active:shadow-inner active:text-white transition-all relative group border-l-4 border-gray-300 hover:border-gray-400">
         <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-11/12">
           <div className="text-center select-none">{category.name}</div>
         </div>
