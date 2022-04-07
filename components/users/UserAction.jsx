af4fd0d60e87687c2cfde83e5598c66c80d5c701
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-04-07 20:35:50 +0200

    user action: add select-none

--- components/users/UserAction.jsx
+++ components/users/UserAction.jsx
@@ -7,7 +7,7 @@ const UserAction = ({ href, text, bcgImg }) => {
         <div
           className={`h-14 md:h-28 xl:h-36 ${bcgImg} bg-cover bg-center rounded hover:shadow-lg group transition-all border-2`}
         >
-          <div className="absolute z-10 left-1/2 top-1/2 md:top-1 -translate-x-1/2 -translate-y-1/2 md:translate-y-0 text-center text-xl md:py-1 text-white mb-1 w-4/5 mx-auto md:border-b group-hover:bg-gray-50 group-hover:text-gray-800 group-hover:rounded">
+          <div className="absolute z-10 left-1/2 top-1/2 md:top-1 -translate-x-1/2 -translate-y-1/2 md:translate-y-0 text-center text-xl md:py-1 text-white mb-1 w-4/5 mx-auto md:border-b group-hover:bg-gray-50 group-hover:text-gray-800 group-hover:rounded select-none">
             {text}
           </div>
           <div className="bg-gray-900 h-full bg-opacity-60 relative group-hover:opacity-25 rounded transition-all"></div>
