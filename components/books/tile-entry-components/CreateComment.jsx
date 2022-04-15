7829d00d13ad89ee14be62a1fedb370bb57a54d6
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-04-15 19:32:50 +0200

    create comment: update buttons style

--- components/books/tile-entry-components/CreateComment.jsx
+++ components/books/tile-entry-components/CreateComment.jsx
@@ -19,7 +19,7 @@ const CreateComment = ({ entryId, userId, hideForm, updateCommentsState }) => {
   };
 
   return (
-    <div className="z-30 fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full lg:w-5/6 xl:w-3/5 2xl:w-1/2 h-full lg:h-2/3 bg-white pt-20 pb-10 lg:py-5 px-2 lg:rounded lg:shadow overflow-auto lg:border-2 lg:border-gray-700">
+    <div className="z-30 fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full lg:w-3/5 2xl:w-1/2 h-full lg:h-1/2 bg-white pt-20 pb-10 lg:py-5 px-2 lg:rounded lg:shadow overflow-auto lg:border-2 lg:border-gray-700">
       <div className="flex items-center justify-around border-b py-2 text-gray-700 w-4/5 mx-auto hover:bg-gray-50 active:bg-gray-100 cursor-pointer">
         <Edit strokeWidth={1.5} />
         <div className="text-lg">Write your comment</div>
@@ -36,7 +36,7 @@ const CreateComment = ({ entryId, userId, hideForm, updateCommentsState }) => {
           name="content"
           id="content"
           className="border-none bg-white mt-2 focus:ring-0 w-5/6 mx-auto block"
-          placeholder="... ..."
+          placeholder="Write your comment here"
           rows="15"
           minLength="5"
           onChange={handleChange}
@@ -45,7 +45,7 @@ const CreateComment = ({ entryId, userId, hideForm, updateCommentsState }) => {
         <div className="flex items-center justify-around lg:justify-center lg:gap-x-5 w-11/12 mx-auto mt-5">
           <div
             onClick={hideForm}
-            className="flex items-center justify-between gap-x-2 border p-2 rounded hover:bg-gray-50 cursor-pointer group"
+            className="flex items-center justify-between gap-x-2 border p-2 rounded bg-gray-50 hover:bg-gray-100 cursor-pointer group"
           >
             <XCircle
               strokeWidth={1.5}
@@ -53,7 +53,7 @@ const CreateComment = ({ entryId, userId, hideForm, updateCommentsState }) => {
             />
             <div>discard comment</div>
           </div>
-          <div className="flex items-center justify-between gap-x-2 border p-2 rounded hover:bg-gray-50 cursor-pointer group">
+          <div className="flex items-center justify-between gap-x-2 border p-2 rounded bg-gray-50 hover:bg-gray-100 cursor-pointer group">
             <PlusCircle
               strokeWidth={1.5}
               className="text-gray-700 group-hover:scale-95 group-active:scale-90"
