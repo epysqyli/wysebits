856dbd2c8cb72354b88d93362f8184b8295f8eec
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-04-19 20:42:50 +0200

    remove unnecessary cursor-pointers

--- components/trending/TrendingBook.jsx
+++ components/trending/TrendingBook.jsx
@@ -5,7 +5,7 @@ const TrendingBook = ({ book }) => {
   return (
     <div>
       <Banner text="Book" iconProp="book" />
-      <div className="rounded-md bg-white shadow-lg hover:bg-gray-50 hover:shadow-xl transition-all cursor-pointer active:shadow-inner border-b-2 mt-5 border-blue-200 hover:border-blue-300 py-2">
+      <div className="rounded-md bg-white shadow-lg hover:bg-gray-50 hover:shadow-xl transition-all active:shadow-inner border-b-2 mt-5 border-blue-200 hover:border-blue-300 py-2">
         <BookCard
           bookData={book}
           showAuthorLink={true}
--- pages/categories/[category].jsx
+++ pages/categories/[category].jsx
@@ -120,7 +120,7 @@ const Category = ({
         <HeaderImage name={categoryName} slug={categorySlug} />
         <div className="mx-auto w-4/5 md:w-4/6 lg:w-3/6 xl:w-1/2 2xl:w-1/3 py-20 lg:py-40 2xl:py-48">
           <NoItem message="This category is empty, meaning no books have been assigned to it. Explore books and contribute insights to improve WyseBits." />
-          <div className="border px-5 mt-20 lg:mt-32 bg-gray-50 rounded-md shadow group transition-all cursor-pointer hover:shadow-md">
+          <div className="border px-5 mt-20 lg:mt-32 bg-gray-50 rounded-md shadow group transition-all hover:shadow-md">
             <div className="px-3 pt-5">
               You can assign books to specific categories on the same page you
               write insights, by clicking below the book cover
