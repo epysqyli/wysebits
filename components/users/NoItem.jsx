043bbcf02a47594a2f01f8b7c959f406f1c0a9aa
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-04-24 11:18:37 +0200

    NoItem: restyle

--- components/users/NoItem.jsx
+++ components/users/NoItem.jsx
@@ -3,14 +3,14 @@ import { AlertCircle } from "react-feather";
 const NoItem = ({ message }) => {
   return (
     <>
-      <div className="flex justify-center gap-x-5 items-center mx-auto">
+      <div className="flex justify-center gap-x-2 items-center mx-auto">
         <AlertCircle
           size={36}
           strokeWidth={1.5}
-          className="rounded-3xl w-1/6"
+          className="rounded-3xl w-1/4 text-gray-100"
           fill="lightgray"
         />
-        <div className="text w-4/6 md:text-lg text-gray-100">{message}</div>
+        <div className="w-3/4 md:text-lg text-gray-100 text-center">{message}</div>
       </div>
     </>
   );
