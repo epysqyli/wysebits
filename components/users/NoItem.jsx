4c7c2fdf8269900907b5822ac0cc56b685879b23
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-04-26 23:58:51 +0200

    no item: apply text right

--- components/users/NoItem.jsx
+++ components/users/NoItem.jsx
@@ -5,12 +5,12 @@ const NoItem = ({ message }) => {
     <>
       <div className="flex justify-center gap-x-2 items-center mx-auto">
         <AlertCircle
-          size={36}
+          size={40}
           strokeWidth={1.5}
           className="rounded-3xl w-1/4 text-gray-100"
           fill="transparent"
         />
-        <div className="w-3/4 md:text-lg text-gray-100 text-center">{message}</div>
+        <div className="w-3/4 md:text-lg text-gray-100">{message}</div>
       </div>
     </>
   );
