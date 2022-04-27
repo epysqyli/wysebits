b9fab44ef8d694a580f68a94da7d970544d1ba0a
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-04-27 19:19:29 +0200

    books: no insights message

--- components/users/NoItem.jsx
+++ components/users/NoItem.jsx
@@ -3,14 +3,14 @@ import { AlertCircle } from "react-feather";
 const NoItem = ({ message }) => {
   return (
     <>
-      <div className="flex justify-center gap-x-2 items-center mx-auto">
+      <div className="flex items-center">
         <AlertCircle
           size={40}
           strokeWidth={1.5}
-          className="rounded-3xl w-1/4 text-gray-100"
+          className="rounded-3xl w-1/3 text-gray-100"
           fill="transparent"
         />
-        <div className="w-3/4 md:text-lg text-gray-100">{message}</div>
+        <div className="w-2/3 md:text-lg text-gray-100">{message}</div>
       </div>
     </>
   );
