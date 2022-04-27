efc7f53febc7a2245964090eea526f8fbbd3588c
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-04-27 19:04:22 +0200

    app loader: change style

--- components/navigation/FullScreenLoader.jsx
+++ components/navigation/FullScreenLoader.jsx
@@ -1,14 +1,10 @@
-import { Loader } from "react-feather";
-
 const FullScreenLoader = () => {
   return (
-    <div className="fixed w-full h-screen z-50 bg-gray-200 bg-opacity-25">
-      <div className="w-min animate-spin mx-auto mt-20">
-        <Loader
-          size={40}
-          color="black"
-          className="absolute -translate-x-1/2 -translate-y-1/2 border rounded-full p-2 bg-white"
-        />
+    <div className="fixed w-full h-screen z-50 bg-gray-800 bg-opacity-25">
+      <div class="lds-ellipsis mx-auto scale-150 mt-60">
+        <div></div>
+        <div></div>
+        <div></div>
       </div>
     </div>
   );
