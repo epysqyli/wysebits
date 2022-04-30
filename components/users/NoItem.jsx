c9a66ec6a95403e3bbad352b71fea7ebe6aca3ec
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-04-30 16:48:01 +0200

    no item: adjust width

--- components/users/NoItem.jsx
+++ components/users/NoItem.jsx
@@ -3,14 +3,14 @@ import { AlertCircle } from "react-feather";
 const NoItem = ({ message }) => {
   return (
     <>
-      <div className="flex items-center">
+      <div className="flex items-center mx-auto w-5/6">
         <AlertCircle
           size={48}
           strokeWidth={1.5}
           className="rounded-3xl w-1/4 text-gray-100"
           fill="transparent"
         />
-        <div className="w-3/4 md:text-lg text-gray-100">{message}</div>
+        <div className="w-3/4 md:text-lg text-gray-100 text-right">{message}</div>
       </div>
     </>
   );
