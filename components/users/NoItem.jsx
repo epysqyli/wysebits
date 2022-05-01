db55f38f59f01d1b4dbac147526a9df34035a756
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-05-01 20:05:01 +0200

    no items: fix again

--- components/users/NoItem.jsx
+++ components/users/NoItem.jsx
@@ -3,14 +3,14 @@ import { AlertCircle } from "react-feather";
 const NoItem = ({ message }) => {
   return (
     <>
-      <div className="flex items-center mx-auto w-5/6">
+      <div className="flex items-center justify-between lg:justify-around xl:justify-center mx-auto px-2 py-1">
         <AlertCircle
-          size={48}
+          size={42}
           strokeWidth={1.5}
-          className="rounded-3xl w-1/4 text-gray-100"
+          className="rounded-3xl text-gray-100"
           fill="transparent"
         />
-        <div className="w-3/4 md:text-lg text-gray-100 text-right">{message}</div>
+        <div className="w-2/3 md:text-lg text-gray-100 text-right">{message}</div>
       </div>
     </>
   );
