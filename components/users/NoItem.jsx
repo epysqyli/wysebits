10ad6a1c80f9061f7c159e5fd96229cb6e6d718f
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-05-02 19:10:54 +0200

    no item: remove flex

--- components/users/NoItem.jsx
+++ components/users/NoItem.jsx
@@ -3,14 +3,16 @@ import { AlertCircle } from "react-feather";
 const NoItem = ({ message }) => {
   return (
     <>
-      <div className="flex items-center justify-between lg:justify-around xl:justify-center mx-auto px-2 py-1">
-        <AlertCircle
-          size={42}
-          strokeWidth={1.5}
-          className="rounded-3xl text-gray-100"
-          fill="transparent"
-        />
-        <div className="w-2/3 md:text-lg text-gray-100 text-right">{message}</div>
+      <div className="mx-auto px-2 py-1">
+        <div className="w-min mx-auto mb-10">
+          <AlertCircle
+            size={42}
+            strokeWidth={1.5}
+            className="rounded-3xl text-gray-100"
+            fill="transparent"
+          />
+        </div>
+        <div className="md:text-lg text-gray-100 text-center">{message}</div>
       </div>
     </>
   );
