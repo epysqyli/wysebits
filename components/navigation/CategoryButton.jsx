eb7b1a0679889f177766fa7a7851679dacb437e1
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-05-13 18:08:58 +0200

    category button: change style

--- components/navigation/CategoryButton.jsx
+++ components/navigation/CategoryButton.jsx
@@ -11,10 +11,11 @@ const CategoryButton = ({ category }) => {
         },
       }}
     >
-      <div className="text-gray-900 text-xl tracking-tight h-16 cursor-pointer rounded bg-white hover:bg-gray-50 active:bg-blue-400 active:shadow-inner active:text-white transition-all relative group border-r-4 border-l-4 border-b-4 border-gray-300 hover:border-gray-400">
+      <div className="text-xl tracking-tight h-16 cursor-pointer transition-all relative group shadow-lg text-gray-50 border-gray-50 border-2 hover:bg-gray-50 hover:text-gray-700 hover:border-blue-300 rounded-md">
         <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-11/12">
           <div className="text-center select-none">{category.name}</div>
         </div>
+        <div className="bg-gradient-to-r from-green-200 to-yellow-200 h-full w-full opacity-10"></div>
       </div>
     </Link>
   );
