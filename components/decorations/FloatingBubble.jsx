32e32dc107eb50dcf34ece0847c483b2f5733c8c
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-05-18 19:48:13 +0200

    index: improve animation on feed and style on buttons

--- components/decorations/FloatingBubble.jsx
+++ components/decorations/FloatingBubble.jsx
@@ -3,8 +3,8 @@ import { motion } from "framer-motion";
 const FloatingBubble = ({ addStyle }) => {
   return (
     <motion.div
-      animate={{ margin: [2, 10, 2], scale: [1, 0.75, 1] }}
-      transition={{ repeat: Infinity, duration: 2 }}
+      animate={{ margin: [2, 10, 2], scale: [1, 0.75, 1], rotate: [20, 100, 20] }}
+      transition={{ repeat: Infinity, duration: 2.2 }}
       className={`h-3 w-3 bg-white rounded shadow-xl absolute ${addStyle}`}
     ></motion.div>
   );
--- pages/index.jsx
+++ pages/index.jsx
@@ -82,14 +82,14 @@ const Home = ({
             <FloatingBubble addStyle="-top-4 -right-4" />
             <FloatingBubble addStyle="-top-4 right-5" />
             <FloatingBubble addStyle="-top-4 right-14" />
-            <FloatingBubble addStyle="top-4 right-5" />
-            <FloatingBubble addStyle="top-4 -right-4" />
-            <FloatingBubble addStyle="top-4 right-14" />
-            <FloatingBubble addStyle="top-12 right-14" />
-            <FloatingBubble addStyle="top-12 right-5" />
+            <FloatingBubble addStyle="top-3 right-5" />
+            <FloatingBubble addStyle="top-3 -right-4" />
+            <FloatingBubble addStyle="top-3 right-14" />
+            <FloatingBubble addStyle="top-10 right-14" />
+            <FloatingBubble addStyle="top-10 right-5" />
           </div>
           <Link href="/feed">
-            <div className="mx-auto md:mx-0 my-16 lg:my-20 w-11/12 md:w-3/5 lg:w-2/5 2xl:w-1/4 px-2 py-14 rounded-md bg-white hover:bg-gray-50 active:shadow-inner active:bg-blue-100 transition-colors cursor-pointer">
+            <div className="mx-auto md:mx-0 my-16 lg:my-20 w-11/12 md:w-3/5 lg:w-2/5 2xl:w-1/4 px-2 py-12 rounded-md bg-white hover:bg-gray-50 active:shadow-inner active:bg-blue-100 transition-colors cursor-pointer">
               <div className="text-gray-600 mx-auto">
                 <Layers size={60} strokeWidth={1.5} className="mb-10 mx-auto" />
                 <div className="text-center text-4xl font-medium">
@@ -102,11 +102,11 @@ const Home = ({
             <FloatingBubble addStyle="-bottom-4 -left-4" />
             <FloatingBubble addStyle="-bottom-4 left-5" />
             <FloatingBubble addStyle="-bottom-4 left-14" />
-            <FloatingBubble addStyle="bottom-4 left-5" />
-            <FloatingBubble addStyle="bottom-4 -left-4" />
-            <FloatingBubble addStyle="bottom-4 left-14" />
-            <FloatingBubble addStyle="bottom-12 left-14" />
-            <FloatingBubble addStyle="bottom-12 left-5" />
+            <FloatingBubble addStyle="bottom-3 left-5" />
+            <FloatingBubble addStyle="bottom-3 -left-4" />
+            <FloatingBubble addStyle="bottom-3 left-14" />
+            <FloatingBubble addStyle="bottom-10 left-14" />
+            <FloatingBubble addStyle="bottom-10 left-5" />
           </div>
         </div>
       </div>
