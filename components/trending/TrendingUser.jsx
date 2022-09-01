51928b148678119c08f9d0a4a3709645cbe0c743
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-09-01 20:59:40 +0200

    trending user: remove tiles diff count.

--- components/trending/TrendingUser.jsx
+++ components/trending/TrendingUser.jsx
@@ -12,9 +12,7 @@ const TrendingUser = ({ user }) => {
             <Avatar avatarUrl={user.avatar_url} size={60} />
           </div>
           <div className="w-1/2 mx-auto my-5 text-center">
-            <span className="font-bold">{user.username}</span> is the most
-            prolific user this week, posting new insights for{" "}
-            {user.tiles_count_diff} books. Congrats!
+            <span className="font-bold">{user.username}</span> is the most prolific user
           </div>
         </div>
       </div>
