d7e1775a521a72a75d32732fc417de3d629aade7
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-09-14 22:44:18 +0200

    interfaces: add elastic book result interface.

---
+++ interfaces/IElasticBookResult.ts
@@ -0,0 +1,25 @@
+import IAuthor from "./IAuthor";
+import IElasticTileEntry from "./IElasticTileEntry";
+
+interface IElasticBookResult {
+  _index: string;
+  _type: string;
+  _id: string;
+  _source: {
+    title: string;
+    ol_key: string;
+    cover_url?: string;
+    tiles_count: number;
+    elastic_tile_entries: Array<IElasticTileEntry>;
+  };
+  category: {
+    id: number;
+    slug: string;
+  };
+  authors: Array<IAuthor>;
+  highlight: {
+    title: Array<string>;
+  };
+}
+
+export default IElasticBookResult;
