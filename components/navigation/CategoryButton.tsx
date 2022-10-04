5bcaf1b2ef08627541d6096c0eaea75f89a45b6d
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-10-04 21:18:58 +0200

    category button: remove gradient.

--- components/navigation/CategoryButton.tsx
+++ components/navigation/CategoryButton.tsx
@@ -20,7 +20,7 @@ const CategoryButton = ({ category }: Props) => {
         <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-11/12'>
           <div className='text-center select-none'>{category.name}</div>
         </div>
-        <div className='h-full w-full rounded bg-gradient-to-b from-slate-600 to-slate-600 group-hover:to-slate-700  group-active:to-slate-800'></div>
+        <div className='h-full w-full rounded bg-slate-600 group-hover:bg-slate-700 group-active:bg-slate-800'></div>
       </div>
     </Link>
   );
