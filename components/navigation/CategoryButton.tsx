3e1b6011af158fe2ef7b8d2fb0f53e295d4094b2
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-10-04 21:14:47 +0200

    category button: use different border and rounded style.

--- components/navigation/CategoryButton.tsx
+++ components/navigation/CategoryButton.tsx
@@ -16,11 +16,11 @@ const CategoryButton = ({ category }: Props) => {
         }
       }}
     >
-      <div className='text-xl tracking-tight h-16 cursor-pointer relative text-white border-slate-400 border-2 rounded-md hover:shadow-md group'>
+      <div className='text-xl tracking-tight h-16 cursor-pointer relative text-white border-b-2 border-slate-200 hover:border-slate-50 rounded hover:shadow-md group'>
         <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-11/12'>
           <div className='text-center select-none'>{category.name}</div>
         </div>
-        <div className='h-full w-full rounded-md bg-gradient-to-r from-slate-600 to-slate-700 group-hover:to-slate-800  group-active:to-slate-900'></div>
+        <div className='h-full w-full rounded bg-gradient-to-b from-slate-600 to-slate-600 group-hover:to-slate-700  group-active:to-slate-800'></div>
       </div>
     </Link>
   );
